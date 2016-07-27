731594dc68fdaeff0bec85457d7b0f22f924ff6c
Jake <jake.jerner@gmail.com>
2016-07-27 17:05:04 +0200

    Powerbar fixup

--- playerhud.lua
+++ playerhud.lua
@@ -219,7 +219,7 @@ function update_power_data(self,forcePowerType,powerToken,forceAnimationName)
                 local spark_prec = spark_current - spark_min
                 
                             
-                local spark = math.floor(powerBarWidth*round_closest) - math.floor(15*spark_prec)
+                local spark = math.min(powerBarWidth-15,math.floor(powerBarWidth*round_closest) - math.floor(15*spark_prec))
                 local bI = 17 - math.max(1,intRound(16 * spark_prec))
 
                    _G[self:GetName()..'CandySpark']:SetTexCoord(bloodSpark[bI].left,
