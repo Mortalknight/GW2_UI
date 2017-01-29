474577a2b35c576eaaeb17d1931e865b11a17db7
Mengqyzh <mengqyzh@foxmail.com>
2017-01-30 03:50:53 +0800

    Addes missing string

--- locales/enUS.lua
+++ locales/enUS.lua
@@ -257,6 +257,7 @@ GwLocalization['TRACKER_TIME_REMAINING'] = 'Time Remaining'
 GwLocalization['TRACKER_MORE_ADVENTURES'] = 'More adventures await:' 
 GwLocalization['TRACKER_ACHIEVEMENTS'] = 'Achievements' 
 GwLocalization['TRACKER_EVENTS'] = 'Events' 
+GwLocalization['TRACKER_EVENT'] = 'Event' 
 
 -- Quest View
 GwLocalization['QUEST_VIEW_DECLINE'] = 'Decline'
