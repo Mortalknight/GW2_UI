d22f27ee003d07370438f059961753274c5050bb
Nils89 <nils-reinecke@hotmail.de>
2017-02-06 10:52:11 +0100

    Fix a Orderhallbar bug

--- hud.lua
+++ hud.lua
@@ -1337,6 +1337,8 @@ function gw_create_orderHallBar()
         GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_CATEGORIES_UPDATED");
         GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_ADDED");
         GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_REMOVED");
+		GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_LIST_UPDATE");
+		GwOrderhallBar:RegisterEvent("MISSION_FINISHED");
         GwOrderhallBar:RegisterEvent("UPDATE_BINDINGS");    
             
     end)
@@ -1352,17 +1354,34 @@ function gw_create_orderHallBar()
         GwOrderhallBar:UnregisterEvent("GARRISON_FOLLOWER_CATEGORIES_UPDATED");
         GwOrderhallBar:UnregisterEvent("GARRISON_FOLLOWER_ADDED");
         GwOrderhallBar:UnregisterEvent("GARRISON_FOLLOWER_REMOVED");
+		GwOrderhallBar:UnregisterEvent("GARRISON_FOLLOWER_LIST_UPDATE");
+		GwOrderhallBar:UnregisterEvent("MISSION_FINISHED");
         GwOrderhallBar:UnregisterEvent("UPDATE_BINDINGS");    
             
     end)
     
+	local inOrderHall = C_Garrison.IsPlayerInGarrison(LE_GARRISON_TYPE_7_0);
+    if inOrderHall then
+		GwOrderhallBar:RegisterEvent("CURRENCY_DISPLAY_UPDATE");
+        GwOrderhallBar:RegisterEvent("DISPLAY_SIZE_CHANGED");
+        GwOrderhallBar:RegisterEvent("UI_SCALE_CHANGED");
+        GwOrderhallBar:RegisterEvent("GARRISON_TALENT_COMPLETE");
+        GwOrderhallBar:RegisterEvent("GARRISON_TALENT_UPDATE");
+        GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_CATEGORIES_UPDATED");
+        GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_ADDED");
+        GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_REMOVED");
+		GwOrderhallBar:RegisterEvent("GARRISON_FOLLOWER_LIST_UPDATE");
+		GwOrderhallBar:RegisterEvent("MISSION_FINISHED");
+        GwOrderhallBar:RegisterEvent("UPDATE_BINDINGS");   
+	end
+	
     gw_orderHallBar_OnEvent(GwOrderhallBar)
     
    
 end
 
 function gw_orderHallBar_OnEvent(self,event)
-    
+
     if OrderHallCommandBar then
         OrderHallCommandBar:SetShown(false); 
         OrderHallCommandBar:UnregisterAllEvents()
