6a066417a1730e0966e3835101917af56d643090
Jake <jake.jerner@gmail.com>
2017-08-28 16:21:26 +0200

    More changelogs

--- changelog.lua
+++ changelog.lua
@@ -6,6 +6,8 @@ Absorb number added to health globe.
 
 
 
+M
+
 Paragon Reputation issues has been corrected.
 
 Update movement bar with correct spell IDs for some classes.
@@ -16,4 +18,8 @@ Fixed some translation errors.
 
 Compass should no longer display errors when dead.
 
+North Tag now hides on hover, so as not to obscure the zone text.
 
+Fix for Immersion addon conflict when using the "Hide Minimap" option, where the vanilla Minimap Zone Text UI would display after talking to an NPC.
+
+Addon icons now follow the minimap in a sqare shape  (Note that using a larger minimap will still cause the icons to overlap the minimap.)
