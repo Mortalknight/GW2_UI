fe16e7097d503e1166837c096bd2125f0ef2666d
Jake <jake.jerner@gmail.com>
2017-08-28 16:16:45 +0200

    Added changelogs

---
+++ changelog.lua
@@ -0,0 +1,18 @@
+
+Stancebar can now be fully toggled in combat.
+
+Absorb number added to health globe.
+
+
+
+Paragon Reputation issues has been corrected.
+
+Update movement bar with correct spell IDs for some classes.
+
+Movement bar should now update with more correct information.
+
+Fixed some translation errors.
+
+Compass should no longer display errors when dead.
+
+
