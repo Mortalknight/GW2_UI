febb9f13941d7d50faa7052dbc2f30db8cff80ad
Nils89 <nils-reinecke@hotmail.de>
2018-09-16 23:56:45 +0200

    Legion Table

--- character/reputation_table.lua
+++ character/reputation_table.lua
@@ -14,6 +14,24 @@ T[2158] = "BFA/Voldunai"
 T[2103] = "BFA/Zandalari Empire"
 T[2157] = "BFA/Honorbound"
 
-
+-- Legion
+T[2170] = "Legion/Argus"
+T[2135] = "Legion/Chromie"
+T[2100] = "Legion/Corbyn"
+T[1883] = "Legion/Dreamweaver"
+T[1900] = "Legion/Farondis"
+T[1828] = "Legion/HighMountain"
+T[2097] = "Legion/Ilyssa"
+T[2102] = "Legion/Impus"
+T[2045] = "Legion/Legionfall"
+T[2165] = "Legion/Light Army"
+T[1975] = "Legion/Margoss"
+T[2098] = "Legion/Raynae"
+T[2099] = "Legion/Riverhorn"
+T[1859] = "Legion/ShalDorei"
+T[2101] = "Legion/Sha'leth"
+T[2018] = "Legion/Talon"
+T[1948] = "Legion/Valarjar"
+T[1894] = "Legion/Wardens"
 
 GW.REP_TEXTURES = T
