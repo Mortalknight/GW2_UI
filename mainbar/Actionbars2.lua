c40aa06abf4336bae9d67fe9b37b62285859ff6f
Nils89 <nils-reinecke@hotmail.de>
2018-10-12 22:06:46 +0200

    Fix ClassPowerBar was not showing after vehicle

--- mainbar/Actionbars2.lua
+++ mainbar/Actionbars2.lua
@@ -498,7 +498,7 @@ local function main_OnEvent(self, event, ...)
     elseif event == "PLAYER_EQUIPMENT_CHANGED" then
         actionBarEquipUpdate()
     elseif unit == "player" and (event == "UNIT_ENTERED_VEHICLE" or event == "UNIT_EXITED_VEHICLE") then
-        if event == "UNIT_ENTERED_VEHICLE" and OverrideActionBar:IsShown() then
+        if event == "UNIT_ENTERED_VEHICLE" and (UnitHasVehiclePlayerFrameUI("player") or OverrideActionBar:IsShown()) then
             toggleMainHud(false, nil)
         else
             toggleMainHud(true, nil)
