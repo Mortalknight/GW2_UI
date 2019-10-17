f054920cd971e8b10fab3cafd317b8756d5cf7c0
Nils89 <nils-reinecke@hotmail.de>
2019-10-17 19:57:30 +0200

    fixup

--- immersive/mainmenu.lua
+++ immersive/mainmenu.lua
@@ -230,18 +230,22 @@ local function SkinUIDropDownMenu_Initialize(self)
     if self.Middle then self.Middle:Hide() end
     if self.Right then self.Right:Hide() end
 
-    self.Button.NormalTexture:SetTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
-    self.Button:SetPushedTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
-    self.Button:SetDisabledTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
-    self.Button:SetHighlightTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
+    if self.Button then
+        self.Button.NormalTexture:SetTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
+        self.Button:SetPushedTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
+        self.Button:SetDisabledTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
+        self.Button:SetHighlightTexture("Interface\\AddOns\\GW2_UI\\textures\\arrowdown_down")
+    end
 
-    local tex = self:CreateTexture("bg", "BACKGROUND")
-    tex:SetPoint("TOP", self, "TOP", 0, 0)
-    tex:SetTexture("Interface\\AddOns\\GW2_UI\\textures\\gwstatusbar")
-    tex:SetPoint("TOPLEFT", self.Left, "BOTTOMRIGHT", 0, 23)
-    tex:SetPoint("BOTTOMRIGHT", self.Right, "TOPLEFT", 10, -20)
-    tex:SetVertexColor(0, 0, 0)
-    self.tex = tex
+    if self.Left and self.Right then
+        local tex = self:CreateTexture("bg", "BACKGROUND")
+        tex:SetPoint("TOP", self, "TOP", 0, 0)
+        tex:SetTexture("Interface\\AddOns\\GW2_UI\\textures\\gwstatusbar")
+        tex:SetPoint("TOPLEFT", self.Left, "BOTTOMRIGHT", 0, 23)
+        tex:SetPoint("BOTTOMRIGHT", self.Right, "TOPLEFT", 10, -20)
+        tex:SetVertexColor(0, 0, 0)
+        self.tex = tex
+    end
 end
 
 local function SkinUIDropDownMenu()
