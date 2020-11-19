4e8fa618c2d51eb97a7903bc4e5ef371a0e9adb0
Kris K <kris.klink@gmail.com>
2020-11-18 18:18:14 -0700

    Updated to skin Postal's addon buttons

--- immersive/skins/mail.lua
+++ immersive/skins/mail.lua
@@ -324,6 +324,32 @@ local function ClearMailTextures()
     MailFrameTab1:Hide()
 end
 
+local function ReskinPostalIfEnabled()
+    if PostalOpenAllButton then
+        PostalOpenAllButton:SkinButton(false, true)
+        
+        PostalSelectOpenButton:SkinButton(false, true)
+        PostalSelectOpenButton:SetPoint("TOPRIGHT", MailFrameTab2, "TOPRIGHT", -85, -35)
+        PostalSelectOpenButton:SetSize(SendMailCancelButton:GetSize())
+
+        PostalSelectReturnButton:SkinButton(false, true)
+        PostalSelectReturnButton:SetPoint("TOPLEFT", PostalSelectOpenButton, "TOPRIGHT", 2, 0)
+        PostalSelectReturnButton:SetSize(PostalSelectOpenButton:GetSize())
+
+        Postal_ModuleMenuButton:SetNormalTexture("Interface/AddOns/GW2_UI/textures/arrowdown_up")
+        Postal_ModuleMenuButton:SetHighlightTexture("Interface/AddOns/GW2_UI/textures/arrowdown_down")
+        Postal_ModuleMenuButton:SetPushedTexture("Interface/AddOns/GW2_UI/textures/arrowdown_down")
+        Postal_ModuleMenuButton:SetDisabledTexture("Interface/AddOns/GW2_UI/textures/arrowdown_up")
+        Postal_ModuleMenuButton:SetPoint("TOPRIGHT", MailFrame, "TOPRIGHT", -30, 33)
+
+        --Postal_OpenAllMenuButton:SetPoint("TOPRIGHT", MailFrame, "TOPRIGHT", -30, 33)
+        Postal_OpenAllMenuButton:SetNormalTexture("Interface/AddOns/GW2_UI/textures/arrowdown_up")
+        Postal_OpenAllMenuButton:SetHighlightTexture("Interface/AddOns/GW2_UI/textures/arrowdown_down")
+        Postal_OpenAllMenuButton:SetPushedTexture("Interface/AddOns/GW2_UI/textures/arrowdown_down")
+        Postal_OpenAllMenuButton:SetDisabledTexture("Interface/AddOns/GW2_UI/textures/arrowdown_up")
+    end
+end
+
 local function SkinMail()
     local eventFrame = CreateFrame("Frame")
     eventFrame:RegisterEvent("MAIL_SHOW")
@@ -464,5 +490,8 @@ local function SkinMail()
 
     -- hook inbox buttons to close the compose view if we want to look at a message and it's open
     AddOnClickHandlers()
+
+    ReskinPostalIfEnabled()
+
 end
 GW.SkinMail = SkinMail
