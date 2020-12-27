269f674d71155b60261217ab8755c65398c1d091
Nils89 <nils-reinecke@hotmail.de>
2020-12-27 17:32:55 +0100

    tweaks

--- character/paperdoll_equipment.lua
+++ character/paperdoll_equipment.lua
@@ -94,6 +94,30 @@ local numBagSlotFrames = 0
 local selectedInventorySlot = nil
 local durabilityFrame = nil
 
+local function UpdateAzeriteItem(self)
+    if not self.styled then
+        self.styled = true
+
+        self.AzeriteTexture:SetAlpha(0)
+        self.RankFrame.Texture:SetTexture()
+        self.RankFrame.Label:SetFont(UNIT_NAME_FONT, 12, "THINOUTLINED")
+    end
+end
+
+local function UpdateAzeriteEmpoweredItem(self)
+    self.AzeriteTexture:SetAtlas("AzeriteIconFrame")
+    self.AzeriteTexture:ClearAllPoints()
+    self.AzeriteTexture:SetPoint("TOPLEFT", self.AzeriteTexture:GetParent(), "TOPLEFT", 2, -2)
+    self.AzeriteTexture:SetPoint("BOTTOMRIGHT", self.AzeriteTexture:GetParent(), "BOTTOMRIGHT", -2, 2)
+    self.AzeriteTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+    self.AzeriteTexture:SetDrawLayer("BORDER", 1)
+end
+
+local function CorruptionIcon(self)
+    local itemLink = GetInventoryItemLink("player", self:GetID())
+    self.IconOverlay:SetShown(itemLink and IsCorruptedItem(itemLink))
+end
+
 local function setItemButtonQuality(button, quality, itemIDOrLink)
     if quality then
         if quality >= Enum.ItemQuality.Common and GetItemQualityColor(quality) then
@@ -123,6 +147,7 @@ local function updateBagItemButton(button)
     if button.ItemLink == nil then 
         button.ItemLink = GW.getInventoryItemLinkByNameAndId(name, id)
     end
+    button:ResetAzeriteItem()
     
     if textureName then
         SetItemButtonTexture(button, textureName)
@@ -151,8 +176,9 @@ local function updateBagItemButton(button)
 
         setItemLevel(button, quality, button.ItemLink)
         setItemButtonQuality(button, quality, id)
-    end
     
+        button.IconOverlay:SetShown(button.ItemLink and IsCorruptedItem(button.ItemLink))
+    end
 end
 GW.AddForProfiling("paperdoll_equipment", "updateBagItemButton", updateBagItemButton)
 
@@ -218,6 +244,22 @@ local function actionButtonGlobalStyle(self)
     self:SetHighlightTexture("Interface/AddOns/GW2_UI/textures/bag/bagitemborder")
     self:GetHighlightTexture():SetBlendMode("ADD")
     self:GetHighlightTexture():SetAlpha(0.33)
+
+    self.IconOverlay:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+    self.IconOverlay:SetAllPoints(self)
+    self.IconOverlay:SetAtlas("Nzoth-inventory-icon")
+    self.IconOverlay:ClearAllPoints()
+    self.IconOverlay:SetPoint("TOPLEFT", self.IconOverlay:GetParent(), "TOPLEFT", 1, -1)
+    self.IconOverlay:SetPoint("BOTTOMRIGHT", self.IconOverlay:GetParent(), "BOTTOMRIGHT", -1, 1)
+
+    self.AzeriteTexture:SetAtlas("AzeriteIconFrame")
+    self.AzeriteTexture:ClearAllPoints()
+    self.AzeriteTexture:SetPoint("TOPLEFT", self.AzeriteTexture:GetParent(), "TOPLEFT", 2, -2)
+    self.AzeriteTexture:SetPoint("BOTTOMRIGHT", self.AzeriteTexture:GetParent(), "BOTTOMRIGHT", -2, 2)
+    self.AzeriteTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+    self.AzeriteTexture:SetDrawLayer("BORDER", 1)
+
+    UpdateAzeriteItem(self)
 end
 GW.AddForProfiling("paperdoll_equipment", "actionButtonGlobalStyle", actionButtonGlobalStyle)
 
@@ -365,23 +407,6 @@ local function updateItemSlot(self)
     local quality = GetInventoryItemQuality("player", slot)
     setItemButtonQuality(self, quality, GetInventoryItemID("player", slot))
     setItemLevel(self, quality, nil, slot)
-
-    if self.HasPaperDollAzeriteItemOverlay then
-        self:SetAzeriteItem(self.hasItem and ItemLocation:CreateFromEquipmentSlot(slot) or nil)
-        if slot ~= 2 then
-            self.AzeriteTexture:SetSize(68, 56) -- equip slot; default is 57,46
-        else
-            self.AzeriteTexture:SetSize(50, 48) -- neck slot; default is 50,44
-        end
-    end
-
-    if self.CorruptedHighlightTexture then
-        if slot ~= 15 then
-            self.CorruptedHighlightTexture:SetSize(84, 84)
-        else
-            self.CorruptedHighlightTexture:SetSize(70, 70)
-        end
-    end
 end
 GW.AddForProfiling("paperdoll_equipment", "updateItemSlot", updateItemSlot)
 
@@ -727,30 +752,6 @@ local function indicatorAnimation(self)
 end
 GW.AddForProfiling("paperdoll_equipment", "indicatorAnimation", indicatorAnimation)
 
-local function UpdateAzeriteItem(self)
-    if not self.styled then
-        self.styled = true
-
-        self.AzeriteTexture:SetAlpha(0)
-        self.RankFrame.Texture:SetTexture()
-        self.RankFrame.Label:SetFont(UNIT_NAME_FONT, 12, "THINOUTLINED")
-    end
-end
-
-local function UpdateAzeriteEmpoweredItem(self)
-    self.AzeriteTexture:SetAtlas("AzeriteIconFrame")
-    self.AzeriteTexture:ClearAllPoints()
-    self.AzeriteTexture:SetPoint("TOPLEFT", self.AzeriteTexture:GetParent(), "TOPLEFT", 2, -2)
-    self.AzeriteTexture:SetPoint("BOTTOMRIGHT", self.AzeriteTexture:GetParent(), "BOTTOMRIGHT", -2, 2)
-    self.AzeriteTexture:SetTexCoord(0.1, 0.9, 0.1, 0.9)
-    self.AzeriteTexture:SetDrawLayer("BORDER", 1)
-end
-
-local function CorruptionIcon(self)
-    local itemLink = GetInventoryItemLink("player", self:GetID())
-    self.IconOverlay:SetShown(itemLink and IsCorruptedItem(itemLink))
-end
-
 local function grabDefaultSlots(slot, anchor, parent, size)
     slot:ClearAllPoints()
     slot:SetPoint(unpack(anchor))
@@ -761,8 +762,6 @@ local function grabDefaultSlots(slot, anchor, parent, size)
     slot.icon:SetTexCoord(0.07, 0.93, 0.07, 0.93)
     slot.icon:SetAlpha(0.9)
     slot.icon:SetAllPoints(slot)
-    slot.IconOverlay:SetTexCoord(0.07, 0.93, 0.07, 0.93)
-    slot.IconOverlay:SetAllPoints(slot)
     slot.IconBorder:SetTexture("Interface/AddOns/GW2_UI/textures/bag/bagitemborder")
     slot.IconBorder:SetAllPoints(slot)
     slot.IconBorder:SetParent(slot)
@@ -789,16 +788,17 @@ local function grabDefaultSlots(slot, anchor, parent, size)
 
     slot.ignoreSlotCheck = CreateFrame("CheckButton", nil, slot, "GWIgnoreSlotCheck")
 
-    slot.overlayButton = CreateFrame("Button", nil, slot)
-    slot.overlayButton:SetAllPoints()
-    slot.overlayButton:Hide()
-    slot.overlayButton:SetHighlightTexture("Interface/AddOns/GW2_UI/textures/bag/bagitemborder")
-
+    slot.IconOverlay:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+    slot.IconOverlay:SetAllPoints(slot)
     slot.IconOverlay:SetAtlas("Nzoth-inventory-icon")
     slot.IconOverlay:ClearAllPoints()
     slot.IconOverlay:SetPoint("TOPLEFT", slot.IconOverlay:GetParent(), "TOPLEFT", 1, -1)
     slot.IconOverlay:SetPoint("BOTTOMRIGHT", slot.IconOverlay:GetParent(), "BOTTOMRIGHT", -1, 1)
 
+    slot.overlayButton = CreateFrame("Button", nil, slot)
+    slot.overlayButton:SetAllPoints()
+    slot.overlayButton:Hide()
+    slot.overlayButton:SetHighlightTexture("Interface/AddOns/GW2_UI/textures/bag/bagitemborder")
     slot.overlayButton:GetHighlightTexture():SetBlendMode("ADD")
     slot.overlayButton:GetHighlightTexture():SetAlpha(0.33)
     slot.overlayButton.isEquipmentSelected = false
@@ -821,8 +821,8 @@ local function grabDefaultSlots(slot, anchor, parent, size)
     hooksecurefunc(slot, "DisplayAsAzeriteItem", UpdateAzeriteItem)
     hooksecurefunc(slot, "DisplayAsAzeriteEmpoweredItem", UpdateAzeriteEmpoweredItem)
 
-    slot:HookScript('OnShow', CorruptionIcon)
-	slot:HookScript('OnEvent', CorruptionIcon)
+    slot:HookScript("OnShow", CorruptionIcon)
+    slot:HookScript("OnEvent", CorruptionIcon)
 
     EquipSlotList[#EquipSlotList + 1] = slot:GetID()
     slotButtons[#slotButtons + 1] = slot
