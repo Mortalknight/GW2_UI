f15e0b16278e98dc341397e3bdf3ec428872da56
Nils89 <nils-reinecke@hotmail.de>
2021-07-30 21:45:33 +0200

    remove print

--- immersive/skins/covenantSanctum.lua
+++ immersive/skins/covenantSanctum.lua
@@ -4,8 +4,6 @@ local function HandleIconString(self, text)
     if not text then text = self:GetText() end
     if not text or text == "" then return end
 
-    print(text)
-
     local new, count = gsub(text, "|T([^:]-):[%d+:]+|t", "|T%1:14:14:0:0:64:64:5:59:5:59|t")
     if count > 0 then self:SetFormattedText("%s", new) end
 end
