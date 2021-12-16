3bdc55a3f259787af8072026bed0f5e36290d9f7
Nils89 <nils-reinecke@hotmail.de>
2021-12-16 08:06:33 +0100

    fix a warfront and heroicVision error

--- objectives/scenarioWidgets/heroicVisions.lua
+++ objectives/scenarioWidgets/heroicVisions.lua
@@ -1,18 +1,19 @@
 local _, GW = ...
-local ParseCriteria = GW.ParseCriteria
 
 local function addHeroicVisionsData(block, numCriteria)
     if GW.locationData.mapID == 1469 or GW.locationData.mapID == 1470 then -- Heroic Vision for OG and SW
         local info = C_CurrencyInfo.GetCurrencyInfo(1744) --Corrupted Memento
-        numCriteria = numCriteria + 1
-        GW.AddScenarioObjectivesBlock(
-            block,
-            ParseCriteria(info.quantity, 0, info.name),
-            false,
-            numCriteria,
-            "monster",
-            info.quantity
-        )
+        if info then
+            numCriteria = numCriteria + 1
+            GW.AddScenarioObjectivesBlock(
+                block,
+                GW.ParseCriteria(info.quantity, 0, info.name),
+                false,
+                numCriteria,
+                "monster",
+                info.quantity
+            )
+        end
     end
 
     return numCriteria
