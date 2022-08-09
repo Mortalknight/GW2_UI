2730b6dd5d39e4576678ac0f328c7709a0f8a703
Nils89 <nils-reinecke@hotmail.de>
2022-08-09 23:03:31 +0200

    WIP: Mounts, container pages will now be created dynamic Make mounts functions works also for critters

--- character/mounts.lua
+++ character/mounts.lua
@@ -3,217 +3,72 @@ local _, GW = ...
 --[[
  TODO
 Drag and drop (any secure way of handling drag and drop?)
-Summon button (any secure way of handling summon buttons spell attribute?)
 ]]
 
--- Number of mount tabs. if changed setUpPaging needs to include the added tabs aswell as the GwMountsFrame template
-local NUM_MOUNT_TABS = 4
 local NUM_MOUNTS_PER_TAB = 12
 
-local firstLoad = false
-
-local function setUpPaging(self)
-    self.left:SetFrameRef('tab', self.attrDummy)
-    self.left:SetAttribute("_onclick", [=[
-        self:GetFrameRef('tab'):SetAttribute('page', 'left')
-    ]=])
-
-    self.right:SetFrameRef('tab', self.attrDummy)
-    self.right:SetAttribute("_onclick", [=[
-        self:GetFrameRef('tab'):SetAttribute('page', 'right')
-    ]=])
-
-    self.attrDummy:SetFrameRef('container1', self.container1)
-    self.attrDummy:SetFrameRef('container2', self.container2)
-    self.attrDummy:SetFrameRef('container3', self.container3)
-    self.attrDummy:SetFrameRef('container4', self.container4)
-    self.attrDummy:SetFrameRef('left', self.left)
-    self.attrDummy:SetFrameRef('right', self.right)
-    self.attrDummy:SetAttribute('_onattributechanged', ([=[
-        if name ~= 'page' then return end
-
-        local p1 = self:GetFrameRef('container1')
-        local p2 = self:GetFrameRef('container2')
-        local p3 = self:GetFrameRef('container3')
-        local p4 = self:GetFrameRef('container4')
-        local left = self:GetFrameRef('left')
-        local right = self:GetFrameRef('right')
-        local numPages = %s
-        local currentPage = 1
-
-        if value == "left" then
-            if p4:IsVisible() then
-                p4:Hide()
-                p3:Show()
-                currentPage = 3
-            elseif p3:IsVisible() then
-                p3:Hide()
-                p2:Show()
-                currentPage = 2
-            elseif p2:IsVisible() then
-                p2:Hide()
-                p1:Show()
-                currentPage = 1
-            end
-        end
-        if value == "right" then
-            if p1:IsVisible()  then
-                p1:Hide()
-                p2:Show()
-                currentPage = 2
-            elseif p2:IsVisible() then
-                p2:Hide()
-                p3:Show()
-                currentPage = 3
-            elseif p3:IsVisible() then
-                p3:Hide()
-                p4:Show()
-                currentPage = 4
-            end
-        end
-
-        if currentPage >= numPages then
-            right:Hide()
-        else
-            right:Show()
-        end
-        if currentPage == 1 then
-            left:Hide()
-        else
-            left:Show()
-        end
-    ]=]):format(self.tabs))
-    self.attrDummy:SetAttribute('page', 'left')
-end
-
-local function onClick_menuButton(self)
-  self.baseFrame.model:SetCreature(self.creatureID)
-  self.baseFrame.title:SetText(self.creatureName)
-  self.baseFrame.summon:SetAttribute("type1", "spell")
-  self.baseFrame.summon:SetAttribute("type2", "spell")
-  self.baseFrame.summon:SetAttribute("spell",self.spellID)
-
-  if ( self.active and self.creatureID ) then
-			self.baseFrame.summon:SetText(self.petType == "MOUNT" and BINDING_NAME_DISMOUNT or PET_DISMISS);
-		else
-			self.baseFrame.summon:SetText(self.petType == "MOUNT" and MOUNT or SUMMON);
-      print(self.petType == "MOUNT" and MOUNT or SUMMON)
-		end
-end
-
-
--- reusable for both mount and companions
-local function updatePetList(self, petType) -- MOUNT / CRITTER
-
-  --NYI Combat lockdown should not update the list, use event PLAYER_REGEN_ENABLED
-
-  local btnIndex = 0
-  local tabIndex = 1
-
-  self.tabs = 1
-
-  for i=1,GetNumCompanions(petType) do
-
-
-    local creatureID, creatureName, spellID, icon, active = GetCompanionInfo(petType, i);
-
-    local btn =_G[GwMountsList:GetName().."container"..tabIndex..'Actionbutton' .. btnIndex]
-
-    btn.icon:SetTexture(icon)
-    btn.title:SetText(creatureName)
-    btn.creatureName = creatureName
-    btn.creatureID = creatureID
-    btn.mountID = i
-    btn.spellID = spellID
-    btn.active = active
-
-  -- For menu items, if we want them to be clickable action buttons
-  --  btn:SetAttribute("ispickable", true)
-  --  btn:SetAttribute("type1", "spell")
-  --  btn:SetAttribute("type2", "spell")
-  --  btn:SetAttribute("spell", spellID)
-    btn:Show()
-
-
-    -- populate the info panel with the first mount
-    if not firstLoad and i==1 then
-      firstLoad = true
-      onClick_menuButton(btn)
-    end
-
-    -- Handle pagnition
-    btnIndex = btnIndex + 1
-    if btnIndex >= NUM_MOUNTS_PER_TAB then
-      btnIndex = 0
-      tabIndex = tabIndex + 1
-      self.tabs = tabIndex
-    end
-  end
-
-  setUpPaging(self)
-
-end
-
--- TEST FUNCTION will be rewritten to work for both mounts and "critters"
 local function LoadMounts()
     local mountsFrame = CreateFrame("Frame", "GwMountsFrame", GwCharacterWindow, "GwMountsFrame")
-    CreateFrame('Frame', 'GwMountsList', GwMountsFrame, 'GwMountsList')
+    local mountsList = CreateFrame('Frame', 'GwMountsList', GwMountsFrame, 'GwMountsList')
+
+    mountsList.neededContainers = math.ceil(GetNumCompanions("MOUNT") / NUM_MOUNTS_PER_TAB)
+    mountsList.maxNumberOfContainers = mountsList.neededContainers  + 2
 
     --Create summon button and set frame level above the model frame
-    local summonButton = CreateFrame("Button","GwMountSummonButton".."MOUNT",GwMountsFrame, "GwMountSummonButton" )
+    local summonButton = CreateFrame("Button","GwMountSummonButton" .. "MOUNT", GwMountsFrame, "GwMountSummonButton" )
     summonButton:SetFrameLevel(mountsFrame.model:GetFrameLevel()+1)
     summonButton:RegisterForClicks("AnyUp")
+    summonButton:SetScript("OnClick", GW.UserMountCritter)
+    summonButton.selectedButton = nil
     mountsFrame.summon = summonButton
 
-    GwMountsFrame.title:SetFont(DAMAGE_TEXT_FONT, 20, "OUTLINE")
-    GwMountsFrame.title:SetTextColor(0.87, 0.74, 0.29, 1)
+    mountsFrame.title:SetFont(DAMAGE_TEXT_FONT, 20, "OUTLINE")
+    mountsFrame.title:SetTextColor(0.87, 0.74, 0.29, 1)
 
-    GwMountsList.pages:SetFont(DAMAGE_TEXT_FONT, 20, "OUTLINE")
-    GwMountsList.pages:SetTextColor(0.7, 0.7, 0.5, 1)
+    mountsList.pages:SetFont(DAMAGE_TEXT_FONT, 20, "OUTLINE")
+    mountsList.pages:SetTextColor(0.7, 0.7, 0.5, 1)
 
-    --loop tabs and create mount buttons
-    for tab=1,NUM_MOUNT_TABS do
+    GW.CreateMountsPetsContainersWithButtons(mountsList, mountsFrame, mountsList.maxNumberOfContainers, NUM_MOUNTS_PER_TAB, "GwMountsListItem")
 
-      local YPadding = 0
-      for i=0,NUM_MOUNTS_PER_TAB do
+    GW.UpdateMountsCritterList(mountsList, "MOUNT", NUM_MOUNTS_PER_TAB, true)
+    GW.SetUpMoutCritterPaging(mountsList)
 
-        local btn = CreateFrame('Button', GwMountsList:GetName().."container"..tab..'Actionbutton' .. i, _G[GwMountsList:GetName().."container"..tab], 'GwMountsListItem')
+    mountsFrame:RegisterEvent("COMPANION_LEARNED")
+    mountsFrame:RegisterEvent("COMPANION_UNLEARNED")
+    mountsFrame:RegisterEvent("COMPANION_UPDATE")
+    mountsFrame:SetScript("OnEvent", function(self, event)
+        if (event == "COMPANION_LEARNED" or event == "PLAYER_REGEN_ENABLED") and not InCombatLockdown() then
+            -- update needed containers
+            if math.ceil(GetNumCompanions("MOUNT") / NUM_MOUNTS_PER_TAB) > mountsList.neededContainers then
+                local oldMaxNumberOfContainers = mountsList.maxNumberOfContainers
+                mountsList.neededContainers = math.ceil(GetNumCompanions("MOUNT") / NUM_MOUNTS_PER_TAB)
+                mountsList.maxNumberOfContainers = mountsList.neededContainers  + 2
 
-        btn.baseFrame = GwMountsFrame
-        btn.petType = "MOUNT"
-        btn:SetPoint('TOPLEFT', GwMountsList, 'TOPLEFT', 0,YPadding)
-        btn:RegisterForClicks("AnyUp")
-        btn:RegisterForDrag("LeftButton")
-        btn:HookScript("OnClick",onClick_menuButton)
-        btn:Hide()
+                mountsList.attrDummy:SetAttribute("maxNumberOfContainers", mountsList.maxNumberOfContainers)
+                mountsList.attrDummy:SetAttribute("neededContainers", mountsList.neededContainers)
 
-        local zebra = tab % 2
-        btn.title:SetFont(DAMAGE_TEXT_FONT, 14, "OUTLINE")
-        btn.title:SetTextColor(0.7, 0.7, 0.5, 1)
-        btn.bg:SetVertexColor(1, 1, 1, zebra)
-        btn.hover:SetTexture('Interface\\AddOns\\GW2_UI\\textures\\character\\menu-hover')
-        btn:SetNormalTexture(nil)
-        btn:SetText("")
+                GW.CreateMountsPetsContainersWithButtons(mountsList, mountsFrame, mountsList.maxNumberOfContainers, NUM_MOUNTS_PER_TAB, "GwMountsListItem", oldMaxNumberOfContainers + 1)
 
-        YPadding = -44 * (i+1)
-      end
-      YPadding = 0
-    end
+                self.attrDummy:SetAttribute('page', 'left')
+            end
+        elseif event == "COMPANION_LEARNED" and InCombatLockdown() then
+            self:RegisterEvent("PLAYER_REGEN_ENABLED")
+            return
+        end
 
-    updatePetList(GwMountsList,"MOUNT")
+        if not self:IsShown() then return end
+        GW.UpdateMountsCritterList(mountsList, "MOUNT", NUM_MOUNTS_PER_TAB)
+    end)
+    mountsFrame:SetScript("OnSHow", function()
+        GW.UpdateMountsCritterList(mountsList, "MOUNT", NUM_MOUNTS_PER_TAB)
+    end)
+
+    -- GwMountsList event handler for updates during combat
+    mountsList:SetScript("OnEvent", function(_, event)
+        mountsList:UnregisterEvent(event)
+        GW.UpdateMountsCritterList(mountsList, "MOUNT", NUM_MOUNTS_PER_TAB)
+    end)
 
     return mountsFrame
 end
 GW.LoadMounts = LoadMounts
-
---[[
-Required events
-
-COMPANION_LEARNED
-COMPANION_UNLEARNED
-COMPANION_UPDATE
-
-Only needed if we want to display global cooldown in mount list
-SPELL_UPDATE_COOLDOWN
-
-]]
---
+++ character/mountsCritterFunctions.lua
@@ -0,0 +1,212 @@
+local _, GW = ...
+
+local function SetUpMoutCritterPaging(self)
+    self.attrDummy:SetAttribute("maxNumberOfContainers", self.maxNumberOfContainers)
+    self.attrDummy:SetAttribute("neededContainers", self.neededContainers)
+
+    self.left:SetFrameRef('tab', self.attrDummy)
+    self.left:SetAttribute("_onclick", [=[
+        self:GetFrameRef('tab'):SetAttribute('page', 'left')
+    ]=])
+
+    self.right:SetFrameRef('tab', self.attrDummy)
+    self.right:SetAttribute("_onclick", [=[
+        self:GetFrameRef('tab'):SetAttribute('page', 'right')
+    ]=])
+
+    self.attrDummy:SetFrameRef('left', self.left)
+    self.attrDummy:SetFrameRef('right', self.right)
+    self.attrDummy:SetAttribute('_onattributechanged', ([=[
+        if name ~= 'page' then return end
+
+        local maxNumberOfContainers = self:GetAttribute("maxNumberOfContainers")
+        local container
+        local showNext = false
+        local hadSomethingToShow = false
+
+        local left = self:GetFrameRef("left")
+        local right = self:GetFrameRef("right")
+
+        local currentPage = 1
+        local neededContainers = self:GetAttribute("neededContainers")
+
+        if value == "left" then -- container -1
+            for i = maxNumberOfContainers, 1, -1 do
+                container = self:GetFrameRef("container" .. i)
+
+                if container:IsVisible() then
+                    container:Hide()
+                    showNext = true
+                else
+                    if showNext then
+                        container:Show()
+                        showNext = false
+                        hadSomethingToShow = true
+                        currentPage = i
+                    else
+                        container:Hide()
+                    end
+                end
+
+                if i == 1 and not hadSomethingToShow then
+                    self:GetFrameRef("container" .. i):Show()
+                    currentPage = i
+                end
+            end
+
+        end
+        if value == "right" then -- container +1
+            for i = 1, maxNumberOfContainers do
+                container = self:GetFrameRef("container" .. i)
+
+                if container:IsVisible() then
+                    container:Hide()
+                    showNext = true
+                else
+                    if showNext then
+                        container:Show()
+                        showNext = false
+                        currentPage = i
+                    else
+                        container:Hide()
+                    end
+                end
+            end
+        end
+
+        if currentPage >= neededContainers then
+            right:Hide()
+        else
+            right:Show()
+        end
+        if currentPage == 1 then
+            left:Hide()
+        else
+            left:Show()
+        end
+    ]=]))
+    self.attrDummy:SetAttribute('page', 'left')
+end
+GW.SetUpMoutCritterPaging = SetUpMoutCritterPaging
+
+local function CreateMountsPetsContainersWithButtons(listFrame, baseFrame, maxNumberOfContainers, numMountsCritterPerTab, listItemTemplate, startTab)
+    -- create dynamic container min 10 to have enough start space
+    for i = 1, maxNumberOfContainers do
+        local container = CreateFrame("Frame", listFrame:GetName() .. "Container" .. i, listFrame, "GWMountCritterListContainerTemplate")
+        container:SetParent(listFrame)
+        container:SetScript("OnShow", function()
+            listFrame.pages:SetText(i)
+        end)
+        listFrame["container" .. i] = container
+        listFrame.attrDummy:SetFrameRef("container" .. i, listFrame["container" .. i])
+
+        if i == 1 then
+            container:Show()
+        end
+    end
+
+    --loop tabs and create mount buttons
+    for tab = (startTab or 1), maxNumberOfContainers do
+        local YPadding = 0
+        for i = 0, numMountsCritterPerTab do
+
+            local btn = CreateFrame('Button', listFrame:GetName() .. "Container"..tab..'Actionbutton' .. i, _G[listFrame:GetName() .. "Container"..tab], listItemTemplate)
+
+            btn.baseFrame = baseFrame
+            btn.baseList = listFrame
+            btn.petType = "MOUNT"
+            btn:SetPoint('TOPLEFT', listFrame, 'TOPLEFT', 0,YPadding)
+            btn:RegisterForClicks("AnyUp")
+            btn:RegisterForDrag("LeftButton")
+            btn:HookScript("OnClick", GW.MountCrintterItemOnClick)
+            btn:Hide()
+
+            local zebra = tab % 2
+            btn.title:SetFont(DAMAGE_TEXT_FONT, 14, "OUTLINE")
+            btn.title:SetTextColor(0.7, 0.7, 0.5, 1)
+            btn.bg:SetVertexColor(1, 1, 1, zebra)
+            btn.hover:SetTexture('Interface\\AddOns\\GW2_UI\\textures\\character\\menu-hover')
+            btn:SetNormalTexture(nil)
+            btn:SetText("")
+
+            YPadding = -44 * (i + 1)
+        end
+        YPadding = 0
+    end
+end
+GW.CreateMountsPetsContainersWithButtons = CreateMountsPetsContainersWithButtons
+
+local function MountCrintterItemOnClick(self)
+    self.baseFrame.model:SetCreature(self.creatureID)
+    self.baseFrame.title:SetText(self.creatureName)
+    self.baseFrame.selectedButton = self
+
+    if self.active and self.creatureID then
+		self.baseFrame.summon:SetText(self.petType == "MOUNT" and BINDING_NAME_DISMOUNT or PET_DISMISS)
+    else
+        self.baseFrame.summon:SetText(self.petType == "MOUNT" and MOUNT or SUMMON)
+    end
+end
+GW.MountCrintterItemOnClick = MountCrintterItemOnClick
+
+local function UserMountCritter(self)
+    local selectedButton = self:GetParent().selectedButton
+
+    if selectedButton.active and selectedButton.creatureID then
+        DismissCompanion(selectedButton.petType )
+        PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON)
+
+		self:SetText(selectedButton.petType  == "MOUNT" and MOUNT or SUMMON)
+    else
+        CallCompanion(selectedButton.petType , selectedButton.mountID)
+		PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_OFF)
+
+        self:SetText(selectedButton.petType == "MOUNT" and BINDING_NAME_DISMOUNT or PET_DISMISS)
+    end
+end
+GW.UserMountCritter = UserMountCritter
+
+local function UpdateMountsCritterList(self, petType, numMountsCritterPerTab, firstLoad) -- MOUNT / CRITTER
+    if InCombatLockdown() then
+        self:RegisterEvent("PLAYER_REGEN_ENABLED")
+        return
+    end
+
+    local btnIndex = 0
+    local tabIndex = 1
+
+    for i = 1, GetNumCompanions(petType) do
+        local creatureID, creatureName, spellID, icon, active = GetCompanionInfo(petType, i)
+
+        local btn =_G[self:GetName() .. "Container" .. tabIndex .. 'Actionbutton' .. btnIndex]
+
+        btn.icon:SetTexture(icon)
+        btn.title:SetText(creatureName)
+        btn.creatureName = creatureName
+        btn.creatureID = creatureID
+        btn.mountID = i
+        btn.spellID = spellID
+        btn.active = active
+
+        -- For menu items, if we want them to be clickable action buttons
+        --  btn:SetAttribute("ispickable", true)
+        --  btn:SetAttribute("type1", "spell")
+        --  btn:SetAttribute("type2", "spell")
+        --  btn:SetAttribute("spell", spellID)
+        btn:Show()
+
+        -- populate the info panel with the first mount
+        if firstLoad and i == 1 then
+            MountCrintterItemOnClick (btn)
+        end
+
+        -- Handle pagnition
+        btnIndex = btnIndex + 1
+        if btnIndex >= numMountsCritterPerTab then
+            btnIndex = 0
+            tabIndex = tabIndex + 1
+            self.tabs = tabIndex
+        end
+    end
+end
+GW.UpdateMountsCritterList = UpdateMountsCritterList
