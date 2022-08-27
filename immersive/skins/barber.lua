bd43722a088b9b12d6c735eadbe33b9371095a97
Nils89 <nils-reinecke@hotmail.de>
2022-08-28 01:18:26 +0200

    fix lua error at barbershop skin and tweak some buttons

--- immersive/skins/barber.lua
+++ immersive/skins/barber.lua
@@ -3,29 +3,53 @@ local constBackdropFrame = GW.skins.constBackdropFrame
 local AFP = GW.AddProfiling
 
 local function hook_SetSelectedCategory(list)
-    for button in list.selectionPopoutPool:EnumerateActive() do
-        if not button.isSkinned then
-            button.DecrementButton:SetNormalTexture("Interface/AddOns/GW2_UI/textures/character/backicon")
-            button.DecrementButton:SetPushedTexture("Interface/AddOns/GW2_UI/textures/character/backicon")
-            button.DecrementButton:SetDisabledTexture("Interface/AddOns/GW2_UI/textures/character/backicon")
-            button.IncrementButton:SetNormalTexture("Interface/AddOns/GW2_UI/textures/character/forwardicon")
-            button.IncrementButton:SetPushedTexture("Interface/AddOns/GW2_UI/textures/character/forwardicon")
-            button.IncrementButton:SetDisabledTexture("Interface/AddOns/GW2_UI/textures/character/forwardicon")
+    if list.selectionPopoutPool then
+        for button in list.selectionPopoutPool:EnumerateActive() do
+            if not button.isSkinned then
+                GW.HandleNextPrevButton(button.DecrementButton)
+                GW.HandleNextPrevButton(button.IncrementButton)
+                button.DecrementButton:SetSize(30, 30)
+                button.IncrementButton:SetSize(30, 30)
 
-            local popoutButton = button.Button
-            popoutButton.Popout:StripTextures()
-            popoutButton.Popout:CreateBackdrop(constBackdropFrame)
-            popoutButton.Popout.backdrop:SetFrameLevel(popoutButton.Popout:GetFrameLevel())
+                -- remove these to fix error on SetHighlightAtlas from AlphaHighlightButtonMixin
+                button.DecrementButton:SetScript("OnMouseUp", nil)
+                button.DecrementButton:SetScript("OnMouseDown", nil)
+                button.IncrementButton:SetScript("OnMouseUp", nil)
+                button.IncrementButton:SetScript("OnMouseDown", nil)
 
-            button.isSkinned = true
+                local popoutButton = button.Button
+                popoutButton.HighlightTexture:SetAlpha(0)
+                popoutButton.NormalTexture:SetAlpha(0)
+
+                popoutButton.Popout:StripTextures()
+                popoutButton.Popout:CreateBackdrop(constBackdropFrame)
+                popoutButton.Popout.backdrop:SetFrameLevel(popoutButton.Popout:GetFrameLevel())
+
+                popoutButton:SkinButton(false, true, false, true)
+                popoutButton:CreateBackdrop(GW.constBackdropFrameColorBorder)
+                popoutButton.backdrop:SetBackdropBorderColor(0, 0, 0)
+                popoutButton.backdrop:SetInside(nil, 4, 4)
+
+                popoutButton:HookScript("OnEnter", function()
+                    popoutButton.backdrop:SetBackdropBorderColor(1, 1, 1)
+                end)
+                popoutButton:HookScript("OnLeave", function()
+                    popoutButton.backdrop:SetBackdropBorderColor(0, 0, 0)
+                end)
+
+                button.isSkinned = true
+            end
         end
     end
 
-    local optionPool = list.pools:GetPool('CharCustomizeOptionCheckButtonTemplate')
-    for button in optionPool:EnumerateActive() do
-        if not button.isSkinned then
-            button.Button:SkinCheckButton()
-            button.isSkinned = true
+    local optionPool = list.pools and list.pools:GetPool("CharCustomizeOptionCheckButtonTemplate")
+    if optionPool then
+        for button in optionPool:EnumerateActive() do
+            if not button.isSkinned then
+                button.Button:SkinCheckButton()
+                button.Button:SetSize(20, 20)
+                button.isSkinned = true
+            end
         end
     end
 end
@@ -41,13 +65,13 @@ local function SkinBarShop()
     BarberShopFrame.LeftBackgroundOverlay:SetDrawLayer("BACKGROUND", 0)
     BarberShopFrame.RightBackgroundOverlay:SetDrawLayer("BACKGROUND", 0)
 
-    CharCustomizeFrame.SmallButtons.ResetCameraButton:SkinButton(false, true)
-    CharCustomizeFrame.SmallButtons.ZoomOutButton:SkinButton(false, true)
-    CharCustomizeFrame.SmallButtons.ZoomInButton:SkinButton(false, true)
-    CharCustomizeFrame.SmallButtons.RotateLeftButton:SkinButton(false, true)
-    CharCustomizeFrame.SmallButtons.RotateRightButton:SkinButton(false, true)
+    CharCustomizeFrame.SmallButtons.ResetCameraButton:SkinButton(false, false, false, true, false, true)
+    CharCustomizeFrame.SmallButtons.ZoomOutButton:SkinButton(false, false, false, true, false, true)
+    CharCustomizeFrame.SmallButtons.ZoomInButton:SkinButton(false, false, false, true, false, true)
+    CharCustomizeFrame.SmallButtons.RotateLeftButton:SkinButton(false, false, false, true, false, true)
+    CharCustomizeFrame.SmallButtons.RotateRightButton:SkinButton(false, false, false, true, false, true)
 
-    hooksecurefunc(_G.CharCustomizeFrame, "SetSelectedCategory", hook_SetSelectedCategory)
+    hooksecurefunc(CharCustomizeFrame, "SetSelectedCategory", hook_SetSelectedCategory)
 end
 AFP("SkinBarShop", SkinBarShop)
 
