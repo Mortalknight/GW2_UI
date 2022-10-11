745f6ad2ab4108bd10988a8245cb9b5eb74fe181
Nils89 <nils-reinecke@hotmail.de>
2022-10-11 21:44:58 +0200

    update raid debuffs

--- core/raid_debuffs.lua
+++ core/raid_debuffs.lua
@@ -9,130 +9,145 @@ local function SetDefaultOnTheFly(id)
 end
 
 -------------------------------------------------
+-------------------- Dungeons -------------------
+-------------------------------------------------
+	-- Ahn'kahet: The Old Kingdom
+	-- Azjol-Nerub
+	-- Drak'Tharon Keep
+	-- Gundrak
+	-- Halls of Lightning
+	-- Halls of Reflection
+	-- Halls of Stone
+	-- Pit of Saron
+	-- The Culling of Stratholme
+	-- The Forge of Souls
+	-- The Nexus
+	-- The Oculus
+	-- The Violet Hold
+	-- Trial of the Champion
+	-- Utgarde Keep
+	-- Utgarde Pinnacle
+-------------------------------------------------
 -------------------- Phase 1 --------------------
 -------------------------------------------------
--- Karazhan
-    -- Attument the Huntsman
-    SetDefaultOnTheFly(29833) -- Intangible Presence
-    SetDefaultOnTheFly(29711) -- Knockdown
-    -- Moroes
-    SetDefaultOnTheFly(29425) -- Gouge
-    SetDefaultOnTheFly(34694) -- Blind
-    SetDefaultOnTheFly(37066) -- Garrote
-    -- Opera Hall Event
-    SetDefaultOnTheFly(30822) -- Poisoned Thrust
-    SetDefaultOnTheFly(30889) -- Powerful Attraction
-    SetDefaultOnTheFly(30890) -- Blinding Passion
-    -- Maiden of Virtue
-    SetDefaultOnTheFly(29511) -- Repentance
-    SetDefaultOnTheFly(29522) -- Holy Fire
-    SetDefaultOnTheFly(29512) -- Holy Ground
-    -- The Curator
-    -- Terestian Illhoof
-    SetDefaultOnTheFly(30053) -- Amplify Flames
-    SetDefaultOnTheFly(30115) -- Sacrifice
-    -- Shade of Aran
-    SetDefaultOnTheFly(29946) -- Flame Wreath
-    SetDefaultOnTheFly(29947) -- Flame Wreath
-    SetDefaultOnTheFly(29990) -- Slow
-    SetDefaultOnTheFly(29991) -- Chains of Ice
-    SetDefaultOnTheFly(29954) -- Frostbolt
-    SetDefaultOnTheFly(29951) -- Blizzard
-    -- Netherspite
-    SetDefaultOnTheFly(38637) -- Nether Exhaustion (Red)
-    SetDefaultOnTheFly(38638) -- Nether Exhaustion (Green)
-    SetDefaultOnTheFly(38639) -- Nether Exhaustion (Blue)
-    SetDefaultOnTheFly(30400) -- Nether Beam - Perseverence
-    SetDefaultOnTheFly(30401) -- Nether Beam - Serenity
-    SetDefaultOnTheFly(30402) -- Nether Beam - Dominance
-    SetDefaultOnTheFly(30421) -- Nether Portal - Perseverence
-    SetDefaultOnTheFly(30422) -- Nether Portal - Serenity
-    SetDefaultOnTheFly(30423) -- Nether Portal - Dominance
-    SetDefaultOnTheFly(30466) -- Nether Portal - Perseverence
-    -- Chess Event
-    SetDefaultOnTheFly(30529) -- Recently In Game
-    -- Prince Malchezaar
-    SetDefaultOnTheFly(39095) -- Amplify Damage
-    SetDefaultOnTheFly(30898) -- Shadow Word: Pain 1
-    SetDefaultOnTheFly(30854) -- Shadow Word: Pain 2
-    -- Nightbane
-    SetDefaultOnTheFly(37091) -- Rain of Bones
-    SetDefaultOnTheFly(30210) -- Smoldering Breath
-    SetDefaultOnTheFly(30129) -- Charred Earth
-    SetDefaultOnTheFly(30127) -- Searing Cinders
-    SetDefaultOnTheFly(36922) -- Bellowing Roar
--- Gruul's Lair
-    -- High King Maulgar
-    SetDefaultOnTheFly(36032) -- Arcane Blast
-    SetDefaultOnTheFly(11726) -- Enslave Demon
-    SetDefaultOnTheFly(33129) -- Dark Decay
-    SetDefaultOnTheFly(33175) -- Arcane Shock
-    SetDefaultOnTheFly(33061) -- Blast Wave
-    SetDefaultOnTheFly(33130) -- Death Coil
-    SetDefaultOnTheFly(16508) -- Intimidating Roar
-    -- Gruul the Dragonkiller
-    SetDefaultOnTheFly(38927) -- Fel Ache
-    SetDefaultOnTheFly(36240) -- Cave In
-    SetDefaultOnTheFly(33652) -- Stoned
-    SetDefaultOnTheFly(33525) -- Ground Slam
--- Magtheridon's Lair
-    -- Magtheridon
-    SetDefaultOnTheFly(44032) -- Mind Exhaustion
-    SetDefaultOnTheFly(30530) -- Fear
-    SetDefaultOnTheFly(38927) -- Fel Ache
+-- Naxxramas
+    -- Anub'Rekhan
+    SetDefaultOnTheFly(54022) -- Locust Swarm
+    SetDefaultOnTheFly(56098) -- Acid Spit
+    -- Grand Widow Faerlina
+    SetDefaultOnTheFly(54099) -- Rain of Fire
+    SetDefaultOnTheFly(54098) -- Poison Bolt Volley
+    -- Maexxna
+    SetDefaultOnTheFly(54121) -- Necrotic Poison 1
+    SetDefaultOnTheFly(28776) -- Necrotic Poison 2
+    SetDefaultOnTheFly(28622) -- Web Wrap
+    SetDefaultOnTheFly(54125) -- Web Spray
+    -- Noth the Plaguebringer
+    SetDefaultOnTheFly(54835) -- Curse of the Plaguebringer
+    SetDefaultOnTheFly(54814) -- Cripple 1
+    SetDefaultOnTheFly(29212) -- Cripple 2
+    -- Heigan the Unclean
+    SetDefaultOnTheFly(55011) -- Decrepit Fever
+    -- Loatheb
+    SetDefaultOnTheFly(55052) -- Inevitable Doom
+    SetDefaultOnTheFly(55053) -- Deathbloom
+    -- Instructor Razuvious
+    SetDefaultOnTheFly(55550) -- Jagged Knife
+    SetDefaultOnTheFly(55470) -- Unbalancing Strike
+    -- Gothik the Harvester
+    SetDefaultOnTheFly(55646) -- Drain Life
+    SetDefaultOnTheFly(55645) -- Death Plague
+    SetDefaultOnTheFly(28679) -- Harvest Soul
+    -- The Four Horsemen
+    SetDefaultOnTheFly(57369) -- Unholy Shadow
+    SetDefaultOnTheFly(28832) -- Mark of Korth'azz
+    SetDefaultOnTheFly(28835) -- Mark of Zeliek
+    SetDefaultOnTheFly(28833) -- Mark of Blaumeux
+    SetDefaultOnTheFly(28834) -- Mark of Rivendare
+    -- Patchwerk
+    SetDefaultOnTheFly(28801) -- Slime / Not really Encounter related
+    -- Grobbulus
+    SetDefaultOnTheFly(28169) -- Mutating Injection
+    -- Gluth
+    SetDefaultOnTheFly(54378) -- Mortal Wound
+    SetDefaultOnTheFly(29306) -- Infected Wound
+    -- Thaddius
+    SetDefaultOnTheFly(28084) -- Negative Charge (-)
+    SetDefaultOnTheFly(28059) -- Positive Charge (+)
+    -- Sapphiron
+    SetDefaultOnTheFly(28522) -- Icebolt
+    SetDefaultOnTheFly(55665) -- Life Drain
+    SetDefaultOnTheFly(28547) -- Chill 1
+    SetDefaultOnTheFly(55699) -- Chill 2
+    -- Kel'Thuzad
+    SetDefaultOnTheFly(55807) -- Frostbolt 1
+    SetDefaultOnTheFly(55802) -- Frostbolt 2
+    SetDefaultOnTheFly(27808) -- Frost Blast
+    SetDefaultOnTheFly(28410) -- Chains of Kel'Thuzad
+-- The Eye of Eternity
+    -- Malygos
+    SetDefaultOnTheFly(56272) -- Arcane Breath
+    SetDefaultOnTheFly(55853) -- Vortex 1
+    SetDefaultOnTheFly(56263) -- Vortex 2
+    SetDefaultOnTheFly(57407) -- Surge of Power
+    SetDefaultOnTheFly(57429) -- Static Field
+-- The Obsidian Sanctum
+    -- Sartharion
+    SetDefaultOnTheFly(60708) -- Fade Armor
+    SetDefaultOnTheFly(58105) -- Power of Shadron
+    SetDefaultOnTheFly(61248) -- Power of Tenebron
+    SetDefaultOnTheFly(56910) -- Tail Lash
+    SetDefaultOnTheFly(57874) -- Twilight Shift
+    SetDefaultOnTheFly(57632) -- Magma
 -------------------------------------------------
 -------------------- Phase 2 --------------------
 -------------------------------------------------
--- Serpentshrine Cavern
-    -- Hydross the Unstable
-    -- The Lurker Below
-    -- Leotheras the Blind
-    -- Fathom-Lord Karathress
-    -- Morogrim Tidewalker
-    -- Lady Vashj
--- The Eye
-    -- Al'ar
-    -- Void Reaver
-    -- High Astromancer Solarian
-    -- Kael'thas Sunstrider
+-- Ulduar
+    -- Flame Leviathan
+    -- Ignis the Furnace Master
+    -- Razorscale
+    -- XT-002 Deconstructor
+    -- The Assembly of Iron
+    -- Kologarn
+    -- Auriaya
+    -- Hodir
+    -- Thorim
+    -- Freya
+    -- Mimiron
+    -- General Vezax
+    -- Yogg-Saron
+    -- Algalon the Observer
 -------------------------------------------------
 -------------------- Phase 3 --------------------
 -------------------------------------------------
--- The Battle for Mount Hyjal
-    -- Rage Winterchill
-    -- Anetheron
-    -- Kaz'rogal
-    -- Azgalor
-    -- Archimonde
--- Black Temple
-    -- High Warlord Naj'entus
-    -- Supremus
-    -- Shade of Akama
-    -- Teron Gorefiend
-    -- Gurtogg Bloodboil
-    -- Reliquary of Souls
-    -- Mother Shahraz
-    -- Illidari Council
-    -- Illidan Stormrage
+-- Trial of the Crusader
+    -- The Northrend Beasts
+    -- Lord Jaraxxus
+    -- Champions of the Horde
+    -- Champions of the Alliance
+    -- Twin Val'kyr
+    -- Anub'arak
+-- Onyxia’s Lair
+    -- Onyxia
 -------------------------------------------------
 -------------------- Phase 4 --------------------
 -------------------------------------------------
--- Zul'Aman
-    -- Nalorakk
-    -- Jan'alai
-    -- Akil'zon
-    -- Halazzi
-    -- Hexxlord Jin'Zakk
-    -- Zul'jin
+-- Icecrown Citadel
+    -- Lord Marrowgar
+    -- Lady Deathwhisper
+    -- Gunship Battle Alliance
+    -- Gunship Battle Horde
+    -- Deathbringer Saurfang
+    -- Festergut
+    -- Rotface
+    -- Professor Putricide
+    -- Blood Prince Council
+    -- Blood-Queen Lana'thel
+    -- Valithria Dreamwalker
+    -- Sindragosa
+    -- The Lich King
 -------------------------------------------------
 -------------------- Phase 5 --------------------
 -------------------------------------------------
--- Sunwell Plateau
-    -- Kalecgos
-    -- Sathrovarr
-    -- Brutallus
-    -- Felmyst
-    -- Alythess
-    -- Sacrolash
-    -- M'uru
-    -- Kil'Jaeden
+-- The Ruby Sanctum
+    -- Halion
