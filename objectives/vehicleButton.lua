1c19497f517a4292452ffe9bfa3f97ed59e49d60
Nils89 <nils-reinecke@hotmail.de>
2022-10-23 23:00:15 +0200

    fix VehicleSeatIndicator

--- objectives/vehicleButton.lua
+++ objectives/vehicleButton.lua
@@ -1,18 +1,19 @@
 local _, GW = ...
 
-local function SetPosition(_ ,_, anchor)
-    if anchor == "MinimapCluster" or anchor == MinimapCluster then
-        _G.VehicleSeatIndicator:ClearAllPoints()
-        _G.VehicleSeatIndicator:SetPoint("TOPLEFT", VehicleSeatIndicator.gwMover)
+local function SetPosition(_ ,_, relativeTo)
+    local mover = VehicleSeatIndicator.gwMover
+    if mover and relativeTo ~= mover then
+        VehicleSeatIndicator:ClearAllPoints()
+        VehicleSeatIndicator:SetPoint("TOPLEFT", mover)
     end
 end
 
 local function LoadVehicleButton()
     if not VehicleSeatIndicator.PositionVehicleFrameHooked then
-        hooksecurefunc(VehicleSeatIndicator, "SetPoint", SetPosition)
-
         VehicleSeatIndicator:ClearAllPoints()
- 		VehicleSeatIndicator:SetPoint("TOPRIGHT", MinimapCluster, "BOTTOMRIGHT", 0, 0)
+        VehicleSeatIndicator:SetPoint("TOPRIGHT", nil, "BOTTOMRIGHT", 0, 0)
+
+        hooksecurefunc(VehicleSeatIndicator, "SetPoint", SetPosition)
 
         GW.RegisterMovableFrame(VehicleSeatIndicator, BINDING_HEADER_VEHICLE, "VEHICLE_SEAT_POS", "VerticalActionBarDummy", nil, {"default", "scaleable"})
         VehicleSeatIndicator.PositionVehicleFrameHooked = true
