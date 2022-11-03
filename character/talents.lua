895515936989632fc0ac16f99e2101fc84ad6cc2
Nils89 <nils-reinecke@hotmail.de>
2022-11-03 17:14:41 +0100

    part II

--- character/talents.lua
+++ character/talents.lua
@@ -1,7 +1,7 @@
 local _, GW = ...
 local CharacterMenuButton_OnLoad = GW.CharacterMenuButton_OnLoad
 
-local function SetupMenuTreeButtonText(self, text)
+local function UpdateMenuTreeButtonText(self, text)
     self:SetText(text)
 end
 
@@ -9,15 +9,28 @@ local function UpdateMenu(menu)
     local classInfo = C_CreatureInfo.GetClassInfo(GW.myClassID)
 	local specName = select(2, GetSpecializationInfoByID(PlayerUtil.GetCurrentSpecID()))
 
-    SetupMenuTreeButtonText(menu.tree1, classInfo and classInfo.className or UNKNOWN)
-    SetupMenuTreeButtonText(menu.tree2, specName)
+    UpdateMenuTreeButtonText(menu.tree1, classInfo and classInfo.className or UNKNOWN)
+    UpdateMenuTreeButtonText(menu.tree2, specName)
 end
 
 local function SetupTreeContainer(fmTalents, index)
     local container = CreateFrame("Frame", "TreeContainer" .. index, fmTalents, "GwTalentskContainerTab")
+    local configId = ClassTalentFrame.TalentsTab:GetConfigID()
+    local talentTreeId = ClassTalentFrame.TalentsTab:GetTalentTreeID()
+    local excludeStagedChangesForCurrencies = ClassTalentFrame.TalentsTab.excludeStagedChangesForCurrencies
+    local counter = 0
 
-    container.test:SetText("Tree " .. index)
+    if configId and configId > 0 and talentTreeId and talentTreeId > 0 then
+        local treeCurrencyInfo = C_Traits.GetTreeCurrencyInfo(configId, talentTreeId, excludeStagedChangesForCurrencies)
+        counter = treeCurrencyInfo[index].quantity
+    end
 
+    local enabled = counter > 0
+	local textColor = enabled and GREEN_FONT_COLOR or GRAY_FONT_COLOR
+
+    container.currencyLabel:SetText(TALENT_FRAME_CURRENCY_DISPLAY_FORMAT:format(string.upper(fmTalents.menuItems[index]:GetText())))
+    container.currencyAmmount:SetText(counter)
+    container.currencyAmmount:SetTextColor(textColor:GetRGBA())
     return container
 end
 
@@ -25,11 +38,28 @@ local function fmTalentsOnEvent(self, event, ...)
     UpdateMenu(self)
 end
 
-local function LoadTalents(tabContainer)
+local function menuItem_OnClick(self)
+    local menuItems = self:GetParent().menuItems
+    for _, v in pairs(menuItems) do
+        v:ClearNormalTexture()
+    end
+    self:SetNormalTexture("Interface\\AddOns\\GW2_UI\\textures\\character\\menu-hover")
+end
 
+local function LoadTalents(tabContainer)
     local talentContainer = CreateFrame("Frame", "GwTalentFrame", tabContainer, "SecureHandlerStateTemplate, GwTalentsWindow")
     local fmTalents = CreateFrame("Frame", "GwTalentsMenu", talentContainer, "GwTalentsMenu")
     GwCharacterWindow:SetFrameRef("GwTalentsMenu", fmTalents)
+
+    UpdateMenu(fmTalents)
+
+    fmTalents.menuItems = {}
+    fmTalents.menuItems[1] = fmTalents.tree1
+    fmTalents.menuItems[2] = fmTalents.tree2
+
+    CharacterMenuButton_OnLoad(fmTalents.tree1, true)
+    CharacterMenuButton_OnLoad(fmTalents.tree2, false)
+    fmTalents.menuItems[1]:SetNormalTexture("Interface\\AddOns\\GW2_UI\\textures\\character\\menu-hover")
 
     fmTalents.tabContainers = {}
     fmTalents:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
@@ -39,6 +69,9 @@ local function LoadTalents(tabContainer)
     fmTalents.tabContainers[2] = SetupTreeContainer(fmTalents, 2)
 
     fmTalents.tabContainers[1]:Show()
+
+    fmTalents.tree1:HookScript("OnClick", menuItem_OnClick)
+    fmTalents.tree2:HookScript("OnClick", menuItem_OnClick)
 
     fmTalents.tree1:RegisterForClicks("AnyUp")
     fmTalents.tree1:SetFrameRef("GwTalentsMenu", fmTalents)
@@ -88,11 +121,6 @@ local function LoadTalents(tabContainer)
             --update here
         end
     )
-
-    UpdateMenu(fmTalents)
-
-    CharacterMenuButton_OnLoad(fmTalents.tree1, true)
-    CharacterMenuButton_OnLoad(fmTalents.tree2, false)
 
     return talentContainer
 end
