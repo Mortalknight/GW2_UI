9fd4bda6f302735c00ac9b62c14ffc1ba08bdd3b
Nils89 <nils-reinecke@hotmail.de>
2022-11-06 19:13:29 +0100

    Added recipe tracking

--- objectives/achievement.lua
+++ objectives/achievement.lua
@@ -197,7 +197,7 @@ local function updateAchievementLayout(self)
     local shownIndex = 1
     local trackedAchievements = {GetTrackedAchievements()}
 
-    self.header:Hide() 
+    self.header:Hide()
 
     local numQuests = #trackedAchievements
     if GwQuesttrackerContainerAchievement.collapsed then
