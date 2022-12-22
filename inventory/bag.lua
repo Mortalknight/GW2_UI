e096a35908071f763d0e075fbb0d5913ed0fa2ad
Nils89 <nils-reinecke@hotmail.de>
2022-12-22 19:54:23 +0100

    update autosell to prevent taint (hopfully)

--- inventory/bag.lua
+++ inventory/bag.lua
@@ -13,10 +13,6 @@ local BAG_ITEM_COMPACT_SIZE = 32
 local BAG_ITEM_PADDING = 5
 local BAG_WINDOW_SIZE = 480
 
-local IterationCount = 500
-local SellJunkFrame = CreateFrame("FRAME")
-local SellJunkTicker, mBagID, mBagSlot
-
 local settings = {
     bagHeader = {}
 }
@@ -41,96 +37,6 @@ local function UpdateSettings()
     end
 end
 GW.UpdateBagSettings = UpdateSettings
-
--- automaticly vendor junk
-local function StopSelling()
-    if SellJunkTicker then SellJunkTicker:Cancel() end
-    GwBagFrame.smsj:Hide()
-    SellJunkFrame:UnregisterEvent("ITEM_LOCKED")
-    SellJunkFrame:UnregisterEvent("ITEM_UNLOCKED")
-end
-
-local function sellJunk()
-    -- Variables
-    local SoldCount, Rarity, ItemPrice = 0, 0, 0
-    local CurrentItemLink
-
-    -- Traverse bags and sell grey items
-    for BagID = 0, 4 do
-        for BagSlot = 1, C_Container.GetContainerNumSlots(BagID) do
-            CurrentItemLink = C_Container.GetContainerItemLink(BagID, BagSlot)
-            if CurrentItemLink then
-                _, _, Rarity, _, _, _, _, _, _, _, ItemPrice = GetItemInfo(CurrentItemLink)
-                if Rarity == 0 and ItemPrice ~= 0 then
-                    SoldCount = SoldCount + 1
-                    if MerchantFrame:IsShown() then
-                        -- If merchant frame is open, vendor the item
-                        C_Container.UseContainerItem(BagID, BagSlot)
-                        -- Perform actions on first iteration
-                        if SellJunkTicker._remainingIterations == IterationCount then
-                            -- Store first sold bag slot for analysis
-                            if SoldCount == 1 then
-                                mBagID, mBagSlot = BagID, BagSlot
-                            end
-                        end
-                    else
-                        -- If merchant frame is not open, stop selling
-                        StopSelling()
-                        return
-                    end
-                end
-            end
-        end
-    end
-
-    -- Stop selling if no items were sold for this iteration or iteration limit was reached
-    if SoldCount == 0 or SellJunkTicker and SellJunkTicker._remainingIterations == 1 then 
-        StopSelling()
-    end
-end
-
-local function SellJunkFrame_OnEvent(self, event)
-    if event == "MERCHANT_SHOW" then
-        -- Reset variables
-        mBagID, mBagSlot = -1, -1
-        -- Do nothing if shift key is held down
-        if IsShiftKeyDown() then return end
-        -- Cancel existing ticker if present
-        if SellJunkTicker then SellJunkTicker:Cancel() end
-        -- Sell grey items using ticker (ends when all grey items are sold or iteration count reached)
-        SellJunkTicker = C_Timer.NewTicker(0.2, sellJunk, IterationCount)
-        self:RegisterEvent("ITEM_LOCKED")
-        self:RegisterEvent("ITEM_UNLOCKED")
-    elseif event == "ITEM_LOCKED" then
-        GwBagFrame.smsj:Show()
-        self:UnregisterEvent("ITEM_LOCKED")
-    elseif event == "ITEM_UNLOCKED" then
-        self:UnregisterEvent("ITEM_UNLOCKED")
-        -- Check whether vendor refuses to buy items
-        if mBagID and mBagSlot and mBagID ~= -1 and mBagSlot ~= -1 then
-            local itemInfo = C_Container.GetContainerItemInfo(mBagID, mBagSlot)
-            if itemInfo.stackCount and not itemInfo.isLocked then
-                -- Item has been unlocked but still not sold so stop selling
-                StopSelling()
-            end
-        end
-    elseif event == "MERCHANT_CLOSED" then
-        -- If merchant frame is closed, stop selling
-        StopSelling()
-    end
-end
-
-local function setupVendorJunk(active)
-    if active then
-        SellJunkFrame:RegisterEvent("MERCHANT_SHOW")
-        SellJunkFrame:RegisterEvent("MERCHANT_CLOSED")
-        SellJunkFrame:SetScript("OnEvent", SellJunkFrame_OnEvent)
-    else
-        SellJunkFrame:UnregisterEvent("MERCHANT_SHOW")
-        SellJunkFrame:UnregisterEvent("MERCHANT_CLOSED")
-        SellJunkFrame:SetScript("OnEvent", nil)
-    end
-end
 
 local function setBagHeaders()
     for i = 1, 5 do
@@ -966,7 +872,7 @@ local function LoadBag(helpers)
                 dd.vendorGrays.checkbutton:SetChecked(newStatus)
                 SetSetting("BAG_VENDOR_GRAYS", newStatus)
                 GW.UpdateBagSettings()
-                setupVendorJunk(newStatus)
+                GW.SetupVendorJunk(newStatus)
             end
         )
 
@@ -1009,7 +915,7 @@ local function LoadBag(helpers)
         dd.showItemLvl.checkbutton:SetChecked(settings.showItemItemLevel)
         dd.separateBags.checkbutton:SetChecked(settings.separateBags)
 
-        setupVendorJunk(dd.vendorGrays.checkbutton:GetChecked())
+        GW.SetupVendorJunk(dd.vendorGrays.checkbutton:GetChecked())
 
         -- setup bag setting title locals
         dd.compactBags.title:SetText(L["Compact Icons"])
