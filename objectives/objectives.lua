f0983fdc8a2be79766ee9387cd8a415ae8542681
Nils89 <nils-reinecke@hotmail.de>
2023-01-14 16:52:01 +0100

    m+ timer again

--- objectives/objectives.lua
+++ objectives/objectives.lua
@@ -122,7 +122,7 @@ local function ParseCriteria(quantity, totalQuantity, criteriaString, isMythicKe
     if quantity ~= nil and totalQuantity ~= nil and criteriaString ~= nil then
         if isMythicKeystone then
             if isWeightedProgress then
-                return string.format("%.2f% (%d/%d) %s", (mythicKeystoneCurrentValue / totalQuantity * 100), mythicKeystoneCurrentValue, totalQuantity, criteriaString)
+                return string.format("%.2f", (mythicKeystoneCurrentValue / totalQuantity * 100)) .."% " ..  string.format("(%s/%s) %s", mythicKeystoneCurrentValue, totalQuantity, criteriaString)
             else
                 return string.format("%d/%d %s", quantity, totalQuantity, criteriaString)
             end
