3498539f4b4a7f0f15afada81eccfeb2f380aaec
Nils89 <nils-reinecke@hotmail.de>
2023-02-23 20:27:54 +0100

    fix icon path

--- objectives/notifications.lua
+++ objectives/notifications.lua
@@ -8,15 +8,15 @@ local notifications = {}
 local questCompass = {}
 
 local icons = {
-    QUEST = {tex = "icon-objective", l = 0, r = 0.5, t = 0.25, b = 0.5},
-    CAMPAIGN = {tex = "icon-objective", l = 0.5, r = 1, t = 0, b = 0.25},
-    EVENT = {tex = "icon-objective", l = 0, r = 0.5, t = 0.5, b = 0.75},
-    SCENARIO = {tex = "icon-objective", l = 0, r = 0.5, t = 0.75, b = 1},
-    BOSS = {tex = "icon-boss", l = 0, r = 1, t = 0, b = 1},
+    QUEST = {tex = "icons/icon-objective", l = 0, r = 0.5, t = 0.25, b = 0.5},
+    CAMPAIGN = {tex = "icons/icon-objective", l = 0.5, r = 1, t = 0, b = 0.25},
+    EVENT = {tex = "icons/icon-objective", l = 0, r = 0.5, t = 0.5, b = 0.75},
+    SCENARIO = {tex = "icons/icon-objective", l = 0, r = 0.5, t = 0.75, b = 1},
+    BOSS = {tex = "icons/icon-boss", l = 0, r = 1, t = 0, b = 1},
     DEAD = {tex = "party/icon-dead", l = 0, r = 1, t = 0, b = 1},
-    ARENA = {tex = "icon-arena", l = 0, r = 1, t = 0, b = 1},
-    DAILY = {tex = "icon-objective", l = 0.5, r = 1, t = 0.25, b = 0.5},
-    TORGHAST = {tex = "icon-objective", l = 0.5, r = 1, t = 0.5, b = 0.75},
+    ARENA = {tex = "icons/icon-arena", l = 0, r = 1, t = 0, b = 1},
+    DAILY = {tex = "icons/icon-objective", l = 0.5, r = 1, t = 0.25, b = 0.5},
+    TORGHAST = {tex = "icons/icon-objective", l = 0.5, r = 1, t = 0.5, b = 0.75},
 }
 
 local notification_priority = {
@@ -354,7 +354,7 @@ local function SetObjectiveNotification()
     end
 
     if icons[data.TYPE] ~= nil then
-        GwObjectivesNotification.icon:SetTexture("Interface/AddOns/GW2_UI/textures/icons/" .. icons[data.TYPE].tex)
+        GwObjectivesNotification.icon:SetTexture("Interface/AddOns/GW2_UI/textures/" .. icons[data.TYPE].tex)
         GwObjectivesNotification.icon:SetTexCoord(icons[data.TYPE].l, icons[data.TYPE].r, icons[data.TYPE].t, icons[data.TYPE].b)
 
         if progress ~= nil and icons[data.TYPE] then
@@ -370,7 +370,7 @@ local function SetObjectiveNotification()
         GwObjectivesNotification.compass.dataIndex = data.ID
 
         if icons[data.TYPE] ~= nil then
-            GwObjectivesNotification.compass.icon:SetTexture("Interface/AddOns/GW2_UI/textures/icons/" .. icons[data.TYPE].tex)
+            GwObjectivesNotification.compass.icon:SetTexture("Interface/AddOns/GW2_UI/textures/" .. icons[data.TYPE].tex)
             GwObjectivesNotification.compass.icon:SetTexCoord(icons[data.TYPE].l, icons[data.TYPE].r, icons[data.TYPE].t, icons[data.TYPE].b)
         else
             GwObjectivesNotification.compass.icon:SetTexture(nil)
