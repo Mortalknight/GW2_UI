68c33cea217803b09b3124ae930efb9dbf47345a
Nils89 <nils-reinecke@hotmail.de>
2024-01-19 10:25:13 +0100

    fix raid frame name overlapping

--- units/grid/elements/nameText.lua
+++ units/grid/elements/nameText.lua
@@ -19,6 +19,7 @@ local function UpdateNameSettings(frame)
 
     name:ClearAllPoints()
     name:SetPoint("TOPLEFT", frame, "TOPLEFT", 2, -2)
+    name:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -2, -2)
 
     frame:Tag(name, ("[GW2_Grid:mainTank(%s)][GW2_Grid:leaderIcon(%s)][GW2_Grid:assistIcon(%s)][GW2_Grid:roleIcon(%s)][GW2_Grid:name] [GW2_Grid:realmFlag(%s)]"):format(tostring(frame.showTankIcon), tostring(frame.showLeaderAssistIcon), tostring(frame.showLeaderAssistIcon), tostring(frame.showRoleIcon), frame.showRealmFlags))
     --frame:Tag(name, "[GW2_Grid:name]")
