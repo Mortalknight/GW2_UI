10bbbecae4ab9cd5ee154a9fee0eaa0ff03c5a59
Mortalknight <jake.jerner@gmail.com>
2024-05-07 14:51:11 +0200

    Null check

--- character/windowManager.lua
+++ character/windowManager.lua
@@ -675,13 +675,15 @@ local function CharacterMenuButton_OnLoad(self, odd, addGwHeroPanelFrameRef)
     else
         self:SetNormalTexture("Interface\\AddOns\\GW2_UI\\textures\\character\\menu-bg")
     end
-    self:GetFontString():SetTextColor(1, 1, 1, 1)
-    self:GetFontString():SetShadowColor(0, 0, 0, 0)
-    self:GetFontString():SetShadowOffset(1, -1)
-    self:GetFontString():SetFont(DAMAGE_TEXT_FONT, 14, "")
-    self:GetFontString():SetJustifyH("LEFT")
-    self:GetFontString():SetPoint("LEFT", self, "LEFT", 5, 0)
-
+    if self:GetFontString() then 
+        self:GetFontString():SetTextColor(1, 1, 1, 1)
+        self:GetFontString():SetShadowColor(0, 0, 0, 0)
+        self:GetFontString():SetShadowOffset(1, -1)
+        self:GetFontString():SetFont(DAMAGE_TEXT_FONT, 14, "")
+        self:GetFontString():SetJustifyH("LEFT")
+        self:GetFontString():SetPoint("LEFT", self, "LEFT", 5, 0)
+    end
+    self.hover:SetAlpha(0)
     if addGwHeroPanelFrameRef then
         self:SetFrameRef("GwCharacterWindow", GwCharacterWindow)
     end
