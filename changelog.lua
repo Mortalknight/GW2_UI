4cfc771104f866361d30c00d056e417ef3d92714
Nils89 <nils-reinecke@hotmail.de>
2024-05-07 09:00:29 +0200

    CL

--- changelog.lua
+++ changelog.lua
@@ -6,9 +6,22 @@ local ct = {
   feature=2,
   change=3,
 }
-
 GW.CHANGELOGS_TYPES = ct
 
+
+addChange("1.2.0",{
+    {ct.feature,[=[Added worldmarker keybindsr]=]},
+    {ct.feature,[=[Added GW2 alert system]=]},
+    {ct.feature,[=[Added back unitframe level coloring]=]},
+    {ct.feature,[=[Added Inspect skin]=]},
+    {ct.feature,[=[Added helper frame skin]=]},
+    {ct.feature,[=[Added role icons back to grids]=]},
+    {ct.bug,[=[Fix raidcontrol]=]},
+    {ct.bug,[=[Fix raidcontrol]=]},
+    {ct.bug,[=[Fix currency values]=]},
+    {ct.bug,[=[Fix game menu settings button if game menu skin is disabled]=]},
+    {ct.change,[=[Update tooltips]=]},
+})
 
 addChange("1.1.0",{
     {ct.feature,[=[Added eclipse bar]=]},
