9520fe76750d81f15cac6f81d70200e7bef5a2dc
Nils89 <nils-reinecke@hotmail.de>
2024-06-22 15:07:17 +0200

    fix mover layouts

--- settings/moveableFrameHandler.lua
+++ settings/moveableFrameHandler.lua
@@ -273,12 +273,12 @@ local function Grid_Show_Hide(self)
 end
 
 local function UpdateMatchingLayout(self, new_point)
-    local selectedLayoutId = GwSmallSettingsContainer.layoutView.savedLayoutDropDown.button.selectedId
-    local layout = selectedLayoutId and GW.GetLayoutById(selectedLayoutId) or nil
+    local selectedLayoutName = GwSmallSettingsContainer.layoutView.savedLayoutDropDown.button.selectedName
+    local layout = selectedLayoutName and GW.GetLayoutByName(selectedLayoutName) or nil
     local frameFound = false
     if layout then
         for i = 0, #layout.frames do
-            if layout.frames[i].settingName == self.setting then
+            if layout.frames[i] and layout.frames[i].settingName == self.setting then
                 layout.frames[i].point = nil
                 layout.frames[i].point = GW.copyTable(nil, new_point)
 
