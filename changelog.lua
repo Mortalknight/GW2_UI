47fba96488b08fe47305420f478e83604d6dac3e
Nils89 <nils-reinecke@hotmail.de>
2024-08-03 11:30:37 +0200

    Fix for Blizzards QuestTimer change

--- changelog.lua
+++ changelog.lua
@@ -2,54 +2,57 @@ local _, GW = ...
 local addChange = GW.addChange
 
 local ct = {
-  bug=1,
-  feature=2,
-  change=3,
+    bug=1,
+    feature=2,
+    change=3,
 }
 GW.CHANGELOGS_TYPES = ct
 
+addChange("3.3.6",{
+    {ct.change,[=[Fix for Blizzards QuestTimer change]=]},
+})
 addChange("3.3.5",{
-  {ct.change,[=[Bagnator support]=]},
+    {ct.change,[=[Bagnator support]=]},
 })
 
 addChange("3.3.4",{
-  {ct.change,[=[Fix for 1.15.3]=]},
+    {ct.change,[=[Fix for 1.15.3]=]},
 })
 
 addChange("3.3.3",{
-  {ct.bug,[=[Added a new spellbook container]=]},
+    {ct.bug,[=[Added a new spellbook container]=]},
 })
 
 addChange("3.3.2",{
-  {ct.bug,[=[Fix target frame and tooltip inspection errors]=]},
+    {ct.bug,[=[Fix target frame and tooltip inspection errors]=]},
 })
 
 addChange("3.3.1",{
-  {ct.bug,[=[Fix lua error on load]=]},
+    {ct.bug,[=[Fix lua error on load]=]},
 })
 
 addChange("3.3.0",{
-  {ct.feature,[=[Added Talent Preview toggle]=]},
+    {ct.feature,[=[Added Talent Preview toggle]=]},
 })
 
 addChange("3.2.0",{
-  {ct.feature,[=[Added SoD dual spec support]=]},
+    {ct.feature,[=[Added SoD dual spec support]=]},
 })
 
 addChange("3.1.1",{
-  {ct.bug,[=[Fix details profile]=]},
+    {ct.bug,[=[Fix details profile]=]},
 })
 
 addChange("3.1.0",{
-  {ct.feature,[=[Added Actionbutton masque skin]=]},
-  {ct.change,[=[Portover chatframe changes]=]},
-  {ct.bug,[=[Fix some bugs]=]},
+    {ct.feature,[=[Added Actionbutton masque skin]=]},
+    {ct.change,[=[Portover chatframe changes]=]},
+    {ct.bug,[=[Fix some bugs]=]},
 })
 
 addChange("3.0.1",{
-  {ct.bug,[=[Fix target frame level color]=]},
-  {ct.bug,[=[Fix targetframe combopoints]=]},
-  {ct.bug,[=[Fix MP5 bar]=]},
+    {ct.bug,[=[Fix target frame level color]=]},
+    {ct.bug,[=[Fix targetframe combopoints]=]},
+    {ct.bug,[=[Fix MP5 bar]=]},
 })
 
 addChange("3.0.0",{
--- immersive/skins/questTimers.lua
+++ immersive/skins/questTimers.lua
@@ -1,8 +1,6 @@
 local _, GW = ...
 
-local function LoadQuestTimersSkin()
-    if not GW.GetSetting("QUESTTIMERS_SKIN_ENABLED") then return end
-
+local function ApplyQuestTimersSkin()
     GW.RegisterMovableFrame(QuestTimerFrame, QUEST_TIMERS, "QUEST_TIMERS_FRAME_POSITION", ALL .. ",BLIZZARD", nil, {"default", "scaleable"})
     QuestTimerFrame:ClearAllPoints()
     QuestTimerFrame:SetPoint("TOPLEFT", QuestTimerFrame.gwMover)
@@ -11,12 +9,18 @@ local function LoadQuestTimersSkin()
     QuestTimerFrame:CreateBackdrop(GW.skins.constBackdropFrameSmallerBorder)
     QuestTimerHeader:SetPoint('TOP', 1, 8)
 
-
     hooksecurefunc(QuestTimerFrame, "SetPoint", function(_, _, parent)
-		if parent ~= QuestTimerFrame.gwMover then
-			QuestTimerFrame:ClearAllPoints()
-			QuestTimerFrame:SetPoint("TOPLEFT", QuestTimerFrame.gwMover)
-		end
-	end)
+        if parent ~= QuestTimerFrame.gwMover then
+            QuestTimerFrame:ClearAllPoints()
+            QuestTimerFrame:SetPoint("TOPLEFT", QuestTimerFrame.gwMover)
+        end
+    end)
+end
+
+local function LoadQuestTimersSkin()
+    if not GW.GetSetting("QUESTTIMERS_SKIN_ENABLED") then return end
+    GW.RegisterLoadHook(ApplyQuestTimersSkin, "Blizzard_QuestTimer", QuestTimerFrame)
+
+    C_AddOns.LoadAddOn("Blizzard_QuestTimer")
 end
 GW.LoadQuestTimersSkin = LoadQuestTimersSkin
