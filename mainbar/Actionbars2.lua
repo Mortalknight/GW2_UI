34260ab0f2b725920127a3d80da6d4dcc8c44b73
Mortalknight <jake.jerner@gmail.com>
2024-09-10 17:20:51 +0200

    Actionbars?

--- mainbar/Actionbars2.lua
+++ mainbar/Actionbars2.lua
@@ -427,7 +427,7 @@ local function FixHotKeyPosition(button, isStanceButton, isPetButton, isMainBar)
     elseif isMainBar then
         button.HotKey:SetPoint("BOTTOMLEFT", button, "BOTTOMLEFT", 0, 0)
         button.HotKey:SetPoint("BOTTOMRIGHT", button, "BOTTOMRIGHT", 0, 0)
-        button.HotKey:SetFont(DAMAGE_TEXT_FONT, 14, "OUTLINED")
+        button.HotKey:SetFont(DAMAGE_TEXT_FONT, 16, "OUTLINED")
         button.HotKey:SetTextColor(1, 1, 1)
     else
         button.HotKey:SetPoint("CENTER", button, "BOTTOM", 0, 0)
@@ -450,7 +450,7 @@ local function setActionButtonStyle(buttonName, noBackDrop, isStanceButton, isPe
         btn.Count:ClearAllPoints()
         btn.Count:SetPoint("TOPRIGHT", btn, "TOPRIGHT", -3, -3)
         btn.Count:SetJustifyH("RIGHT")
-        btn.Count:SetFont(UNIT_NAME_FONT, 14, "OUTLINED")
+        btn.Count:SetFont(UNIT_NAME_FONT, 16, "OUTLINED")
         btn.Count:SetTextColor(1, 1, 0.6)
     end
 
@@ -755,7 +755,7 @@ local function skinMainBar()
 
             hotkey:SetPoint("BOTTOMLEFT", btn, "BOTTOMLEFT", 0, 0)
             hotkey:SetPoint("BOTTOMRIGHT", btn, "BOTTOMRIGHT", 0, 0)
-            hotkey:SetFont(DAMAGE_TEXT_FONT, 14, "OUTLINED")
+            hotkey:SetFont(DAMAGE_TEXT_FONT, 16, "OUTLINED")
             hotkey:SetTextColor(1, 1, 1)
 
             if IsEquippedAction(btn.action) then
@@ -829,7 +829,7 @@ local function updateMainBar()
 
             hotkey:SetPoint("BOTTOMLEFT", btn, "BOTTOMLEFT", 0, 0)
             hotkey:SetPoint("BOTTOMRIGHT", btn, "BOTTOMRIGHT", 0, 0)
-            hotkey:SetFont(DAMAGE_TEXT_FONT, 14, "OUTLINED")
+            hotkey:SetFont(DAMAGE_TEXT_FONT, 16, "OUTLINED")
             hotkey:SetTextColor(1, 1, 1)
             btn.changedColor = false
             btn.rangeIndicatorSetting = GW.settings.MAINBAR_RANGEINDICATOR
