5a4b8b5d7f018cef19fa8c3375841fb3002fd81a
Nils89 <nils-reinecke@hotmail.de>
2024-10-05 00:07:40 +0200

    fix jails tower lua error

--- objectives/scenarioWidgets/jailersTower.lua
+++ objectives/scenarioWidgets/jailersTower.lua
@@ -24,64 +24,19 @@ local function addJailersTowerData(block, numCriteria)
         objectiveBlock:HookScript("OnLeave", GameTooltip_Hide)
         objectiveBlock:SetHeight(objectiveBlock:GetHeight() + 10)
 
-        -- grab new bar
-        numCriteria = numCriteria + 1
-        objectiveBlock = GW.GetScenarioObjectivesBlock(block, numCriteria)
-        TopScenarioWidgetContainerBlock.gwBlock = objectiveBlock
-        objectiveBlock:SetHeight(max(TopScenarioWidgetContainerBlock.height, TopScenarioWidgetContainerBlock.WidgetContainer:GetHeight(), 45))
-        TopScenarioWidgetContainerBlock.SetParent = nil
-        TopScenarioWidgetContainerBlock.ClearAllPoints = nil
-        TopScenarioWidgetContainerBlock.SetPoint = nil
-        TopScenarioWidgetContainerBlock:SetParent(objectiveBlock)
-        TopScenarioWidgetContainerBlock:ClearAllPoints()
-        TopScenarioWidgetContainerBlock:SetPoint("TOP")
-        TopScenarioWidgetContainerBlock.SetParent = GW.NoOp
-        TopScenarioWidgetContainerBlock.ClearAllPoints = GW.NoOp
-        TopScenarioWidgetContainerBlock.SetPoint = GW.NoOp
-        objectiveBlock:Show()
-        objectiveBlock.ObjectiveText:SetText("")
-        block.height = block.height + objectiveBlock:GetHeight()
-        block.numObjectives = block.numObjectives + 1
-        objectiveBlock.hasObjectToHide = true
-        objectiveBlock.objectToHide = TopScenarioWidgetContainerBlock
-        objectiveBlock.resetParent = true
-
         -- grab the MawBuffs Button from here
         numCriteria = numCriteria + 1
         objectiveBlock = GW.GetScenarioObjectivesBlock(block, numCriteria)
-        objectiveBlock:SetHeight(ScenarioBlocksFrame.MawBuffsBlock:GetHeight()) --.Container
-        ScenarioBlocksFrame.MawBuffsBlock.Container:SetParent(objectiveBlock)
-        ScenarioBlocksFrame.MawBuffsBlock.Container:ClearAllPoints()
-        ScenarioBlocksFrame.MawBuffsBlock.Container:SetAllPoints()
+        objectiveBlock:SetHeight(ScenarioObjectiveTracker.MawBuffsBlock:GetHeight()) --.Container
+        ScenarioObjectiveTracker.MawBuffsBlock.Container:SetParent(objectiveBlock)
+        ScenarioObjectiveTracker.MawBuffsBlock.Container:ClearAllPoints()
+        ScenarioObjectiveTracker.MawBuffsBlock.Container:SetAllPoints()
         objectiveBlock:Show()
         objectiveBlock.ObjectiveText:SetText("")
         block.height = block.height + objectiveBlock:GetHeight()
         block.numObjectives = block.numObjectives + 1
         objectiveBlock.hasObjectToHide = true
-        objectiveBlock.objectToHide = ScenarioBlocksFrame.MawBuffsBlock
-        --BottomScenarioWidgetContainerBlock.isHeader
-
-        -- grab new auras
-        numCriteria = numCriteria + 1
-        objectiveBlock = GW.GetScenarioObjectivesBlock(block, numCriteria)
-        BottomScenarioWidgetContainerBlock.gwBlock = objectiveBlock
-        objectiveBlock:SetHeight(max(BottomScenarioWidgetContainerBlock.height, BottomScenarioWidgetContainerBlock.WidgetContainer:GetHeight()))
-        BottomScenarioWidgetContainerBlock.SetParent = nil
-        BottomScenarioWidgetContainerBlock.ClearAllPoints = nil
-        BottomScenarioWidgetContainerBlock.SetPoint = nil
-        BottomScenarioWidgetContainerBlock:SetParent(objectiveBlock)
-        BottomScenarioWidgetContainerBlock:ClearAllPoints()
-        BottomScenarioWidgetContainerBlock:SetPoint("TOP", 0, 15)
-        BottomScenarioWidgetContainerBlock.SetParent = GW.NoOp
-        BottomScenarioWidgetContainerBlock.ClearAllPoints = GW.NoOp
-        BottomScenarioWidgetContainerBlock.SetPoint = GW.NoOp
-        objectiveBlock:Show()
-        objectiveBlock.ObjectiveText:SetText("")
-        block.height = block.height + objectiveBlock:GetHeight() + 15
-        block.numObjectives = block.numObjectives + 1
-        objectiveBlock.hasObjectToHide = true
-        objectiveBlock.objectToHide = BottomScenarioWidgetContainerBlock
-        objectiveBlock.resetParent = true
+        objectiveBlock.objectToHide = ScenarioObjectiveTracker.MawBuffsBlock
     end
 
     return numCriteria
