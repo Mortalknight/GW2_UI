dc42f1813da0d933997d2b359408342a6df1e9e0
Nils89 <nils-reinecke@hotmail.de>
2024-10-29 13:14:08 +0100

    update addon skin

--- immersive/skins/addonManager.lua
+++ immersive/skins/addonManager.lua
@@ -1,5 +1,44 @@
 local _, GW = ...
 
+local function HandleButton(entry, addonIndex)
+    if not entry.IsSkinned then
+        entry.Enabled:GwSkinCheckButton()
+        entry.Enabled:SetSize(15, 15)
+        entry.Enabled:SetHitRectInsets(0, 0, 0, 0)
+        entry.LoadAddonButton:GwSkinButton(false, true)
+        entry.LoadAddonButton.gwBorderFrame:Hide()
+
+        entry.Title:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.NORMAL)
+        entry.Status:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.SMALL)
+        entry.Reload:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.SMALL)
+        entry.Reload:SetTextColor(1.0, 0.3, 0.3)
+        entry.LoadAddonButton.Text:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.SMALL)
+
+        entry.IsSkinned = true
+    end
+
+    local checkstate = C_AddOns.GetAddOnEnableState(addonIndex)
+    if checkstate == 2 then
+        entry.Status:SetTextColor(0.7, 0.7, 0.7)
+    else
+        entry.Status:SetTextColor(0.4, 0.4, 0.4)
+    end
+
+    local _, _, _, _, reason = C_AddOns.GetAddOnInfo(addonIndex)
+    local checktex = entry.Enabled:GetCheckedTexture()
+    if reason == "DEP_DISABLED" then
+        checktex:SetVertexColor(0.3, 0.3, 0.3)
+        checktex:SetDesaturated(true)
+    elseif checkstate == 1 then
+        checktex:SetVertexColor(1, 0.93, 0.73)
+        checktex:SetDesaturated(false)
+    elseif checkstate == 2 then
+        checktex:SetVertexColor(1, 1, 1)
+        checktex:SetDesaturated(false)
+    end
+
+end
+
 local function LoadAddonListSkin()
     if not GW.settings.ADDONLIST_SKIN_ENABLED then return end
     GW.HandlePortraitFrame(AddonList)
@@ -22,64 +61,9 @@ local function LoadAddonListSkin()
     AddonList.CloseButton:ClearAllPoints()
     AddonList.CloseButton:SetPoint("TOPRIGHT", -5, 0)
 
-    GW.HandleTrimScrollBar(AddonList.ScrollBar, true)
+    GW.HandleTrimScrollBar(AddonList.ScrollBar)
     GW.HandleScrollControls(AddonList)
 
-    hooksecurefunc("AddonList_Update", function()
-        for _, entry in next, {AddonList.ScrollBox.ScrollTarget:GetChildren()} do
-            local id = entry:GetID()
-
-            local checkall
-            local character = UIDropDownMenu_GetSelectedValue(AddonList.Dropdown)
-            if character == true then
-                character = nil
-            else
-                checkall = C_AddOns.GetAddOnEnableState(id)
-            end
-
-            entry.Reload:SetTextColor(1.0, 0.3, 0.3)
-
-            local checkstate = C_AddOns.GetAddOnEnableState(id, character)
-            local enabledForSome = not character and checkstate == 1
-            local enabled = checkstate > 0
-            local disabled = not enabled or enabledForSome
-
-            if disabled then
-                entry.Status:SetTextColor(0.4, 0.4, 0.4)
-            else
-                entry.Status:SetTextColor(0.7, 0.7, 0.7)
-            end
-
-            local checktex = entry.Enabled:GetCheckedTexture()
-            if not enabled and checkall == 1 then
-                checktex:SetVertexColor(0.3, 0.3, 0.3)
-                checktex:SetDesaturated(true)
-                checktex:Show()
-            elseif not checkstate or checkstate == 0 then
-                checktex:Hide()
-            elseif checkstate == 1 then
-                checktex:SetVertexColor(1, 0.93, 0.73)
-                checktex:SetDesaturated(true)
-                checktex:Show()
-            elseif checkstate == 2 then
-                checktex:SetVertexColor(1, 1, 1)
-                checktex:SetDesaturated(false)
-                checktex:Show()
-            end
-        end
-    end)
-
-    hooksecurefunc(AddonList.ScrollBox, "Update", function(frame)
-        for _, child in next, {frame.ScrollTarget:GetChildren()} do
-            if not child.IsSkinned then
-                child.Enabled:GwSkinCheckButton()
-                child.Enabled:SetSize(15, 15)
-                child.Enabled:SetHitRectInsets(0, 0, 0, 0)
-                child.LoadAddonButton:GwSkinButton(false, true)
-
-                child.IsSkinned = true
-            end
-        end
-    end)
+    hooksecurefunc("AddonList_InitButton", HandleButton)
 end
 GW.LoadAddonListSkin = LoadAddonListSkin
