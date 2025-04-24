ef71aeee34a4ead0966628d5e14c68c4e53bebd9
Nils89 <nils-reinecke@hotmail.de>
2025-04-24 09:25:38 +0200

    +

--- immersive/skins/perksProgram.lua
+++ immersive/skins/perksProgram.lua
@@ -1,28 +1,45 @@
 local _, GW = ...
 
-local function ReplaceIconString(frame, text)
-    if not text then text = frame:GetText() end
-    if not text or text == "" then return end
-    local newText, count = gsub(text, "|T(%d+):24:24[^|]*|t", " |T%1:16:16:0:0:64:64:5:59:5:59|t")
-    if count > 0 then frame:SetFormattedText("%s", newText) end
+local function HandleCartToggleButton(button)
+    if button.text then
+        button:GwStripTextures()
+        button.text:SetText(button.itemInCart and "-" or "+")
+
+        if button.itemInCart then
+            button.text:SetTextColor(1, 0.3, 0.3)
+        else
+            button.text:SetTextColor(0.3, 1, 0.3)
+        end
+    end
 end
 
-local function HandleRewardButton(button)
-    local container = button.ContentsContainer
+local function HandleRewardButton(child)
+    local container = child.ContentsContainer
     if not container then return end
 
     local icon = container.Icon
     if icon then
         GW.HandleIcon(icon)
+
+        container.IconMask:Hide()
     end
 
-    local price = container.Price
-    if price then
-        ReplaceIconString(price)
-        if not price.IsSkinned then
-            price.IsSkinned = true
-            hooksecurefunc(price, "SetText", ReplaceIconString)
-        end
+    local priceIcon = container.PriceIcon
+    if priceIcon then
+        GW.HandleIcon(priceIcon)
+    end
+    local cartButton = container.CartToggleButton
+    if cartButton and not cartButton.text then
+        cartButton.text = cartButton:CreateFontString(nil, "ARTWORK")
+        cartButton.text:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.BIG_HEADER, "OUTLINE", 14)
+        cartButton.text:SetPoint("CENTER")
+        cartButton.text:SetTextColor(0.3, 1, 0.3)
+        cartButton:GwSkinButton(false, true)
+        cartButton.gwBorderFrame:Hide()
+
+        HandleCartToggleButton(cartButton)
+
+        hooksecurefunc(cartButton, "UpdateCartState", HandleCartToggleButton)
     end
 end
 
@@ -34,11 +51,20 @@ local function HandleSetButtons(button)
         GW.HandleIconBorder(button.IconBorder, button.Icon.backdrop)
     end
 
-    button.BackgroundTexture:SetAlpha(0)
-    button.SelectedTexture:SetColorTexture(1, 0.8, 0, 0.25)
-    button.SelectedTexture:GwSetInside()
-    button.HighlightTexture:SetColorTexture(1, 1, 1, 0.25)
-    button.HighlightTexture:GwSetInside()
+    if button.BackgroundTexture then
+        button.BackgroundTexture:SetAlpha(0)
+    end
+
+    if button.HighlightTexture then
+        button.HighlightTexture:SetColorTexture(1, 1, 1, .25)
+        button.HighlightTexture:GwSetInside()
+    end
+    if button.TopBraceTexture then
+        button.TopBraceTexture:Hide()
+    end
+    if button.BottomBraceTexture then
+        button.BottomBraceTexture:Hide()
+    end
 end
 
 local function HandleSortLabel(button)
@@ -59,9 +85,63 @@ local function DetailsScrollBoxUpdate(box)
     box:ForEachFrame(HandleSetButtons)
 end
 
-local function HandleRewards(box)
-    if box then
-        box:ForEachFrame(HandleRewardButton)
+local function HandleRewards(frame)
+    for _, child in next, { frame.ScrollTarget:GetChildren() } do
+        HandleRewardButton(child)
+    end
+end
+
+local function HandleShoppingCardButtons(button)
+    if not button then return end
+
+    if button.RemoveFromCartItemButton then
+        button.RemoveFromCartItemButton.RemoveFromListButton:GwSkinButton(true)
+    end
+
+    if not button.bgSetTexture then
+        button.bgSetTexture = button:CreateTexture(nil, "BACKGROUND")
+        button.bgSetTexture:SetTexture("Interface/AddOns/GW2_UI/textures/uistuff/white")
+        button.bgSetTexture:GwSetInside(button)
+    end
+
+    if button.Icon and not button.Icon.backdrop then
+        GW.HandleIcon(button.Icon, true)
+        GW.HandleIconBorder(button.IconBorder, button.Icon.backdrop)
+    end
+
+    if button.BackgroundTexture then
+        if not button.BackgroundTexture.backdrop then
+            button.BackgroundTexture:GwStripTextures()
+            button.BackgroundTexture:GwCreateBackdrop(GW.BackdropTemplates.DefaultWithSmallBorder)
+        end
+
+        local color = GW.GetQualityColor(button.elementData and button.elementData.itemQuality)
+        button.bgSetTexture:SetVertexColor(color.r, color.g, color.b, button.elementData and button.elementData.isSetItem and 0.2 or 0)
+    else
+        button.bgSetTexture:SetVertexColor(0, 0, 0, 0.25)
+    end
+
+    if button.TopBraceTexture then
+        button.TopBraceTexture:GwStripTextures()
+    end
+
+    if button.BottomBraceTexture then
+        button.BottomBraceTexture:GwStripTextures()
+    end
+
+    if button.HighlightTexture then
+        button.HighlightTexture:SetColorTexture(1, 1, 1, 0.25)
+    end
+
+    local priceIcon = button.PriceIcon
+    if priceIcon then
+        GW.HandleIcon(priceIcon)
+    end
+end
+
+local function ShoppingCartScrollBoxUpdate(box)
+    for _, child in next, { box.ScrollTarget:GetChildren() } do
+        HandleShoppingCardButtons(child)
     end
 end
 
@@ -107,6 +187,9 @@ local function SkinPerksProgram()
 
     if PerksProgramFrame.ProductsFrame then
         PerksProgramFrame.ProductsFrame.PerksProgramFilter:GwSkinButton(false, true)
+
+        PerksProgramFrame.ProductsFrame.PerksProgramFilter.ResetButton:GwSkinButton(true)
+
         PerksProgramFrame.ProductsFrame.PerksProgramFilter.Text:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.SMALL)
         PerksProgramFrame.ProductsFrame.PerksProgramFilter.Text:SetShadowOffset(0, 0)
         PerksProgramFrame.ProductsFrame.PerksProgramFilter.Text:SetTextColor(0, 0, 0)
@@ -149,6 +232,36 @@ local function SkinPerksProgram()
             hooksecurefunc(container.ScrollBox, "Update", DetailsScrollBoxUpdate)
         end
 
+        local shoppingCart = PerksProgramFrame.ProductsFrame.PerksProgramShoppingCartFrame
+        if shoppingCart then
+            shoppingCart.Background:Hide()
+            shoppingCart:GwStripTextures()
+            shoppingCart:GwCreateBackdrop(GW.BackdropTemplates.DefaultWithSmallBorder)
+            shoppingCart.CloseButton:GwSkinButton(true)
+            shoppingCart.CloseButton:SetFrameLevel(shoppingCart.backdrop:GetFrameLevel() + 1)
+
+            shoppingCart.ClearCartButton:GwSkinButton(false, true)
+            shoppingCart.PurchaseCartButton:GwSkinButton(false, true)
+
+            shoppingCart.ClearCartButton.texture = shoppingCart.ClearCartButton:CreateTexture(nil, "ARTWORK")
+            shoppingCart.ClearCartButton.texture:SetAtlas("Perks-ShoppingCart")
+            shoppingCart.ClearCartButton.texture:GwSetInside(nil, 8, 8)
+            shoppingCart.ClearCartButton.texture:SetDesaturated(true)
+
+            shoppingCart.ClearCartButton.text = shoppingCart.ClearCartButton:CreateFontString(nil, "ARTWORK")
+            shoppingCart.ClearCartButton.text:GwSetFontTemplate(UNIT_NAME_FONT, GW.TextSizeType.BIG_HEADER, "OUTLINE", 24)
+            shoppingCart.ClearCartButton.text:SetPoint("CENTER")
+            shoppingCart.ClearCartButton.text:SetTextColor(1, 0.3, 0.3)
+            shoppingCart.ClearCartButton.text:SetText("/")
+
+            shoppingCart.Title:SetTextColor(1, 1, 1, 1)
+
+            local itemList = shoppingCart.ItemList
+            GW.HandleTrimScrollBar(itemList.ScrollBar)
+            GW.HandleScrollControls(itemList)
+            hooksecurefunc(itemList.ScrollBox, "Update", ShoppingCartScrollBoxUpdate)
+        end
+
         local carousel = PerksProgramFrame.ProductsFrame.PerksProgramProductDetailsContainerFrame.CarouselFrame
         if carousel and carousel.IncrementButton then
             HandleNextPrev(carousel.IncrementButton)
@@ -174,6 +287,9 @@ local function SkinPerksProgram()
         PerksProgramFrame.FooterFrame.LeaveButton:GwSkinButton(false, true)
         PerksProgramFrame.FooterFrame.PurchaseButton:GwSkinButton(false, true)
         PerksProgramFrame.FooterFrame.RefundButton:GwSkinButton(false, true)
+        PerksProgramFrame.FooterFrame.AddToCartButton:GwSkinButton(false, true)
+        PerksProgramFrame.FooterFrame.RemoveFromCartButton:GwSkinButton(false, true)
+        PerksProgramFrame.FooterFrame.ViewCartButton:GwSkinButton(false, true)--, nil, nil, true)
 
         PerksProgramFrame.FooterFrame.RotateButtonContainer.RotateLeftButton:GwSkinButton(false, true)
         PerksProgramFrame.FooterFrame.RotateButtonContainer.RotateRightButton:GwSkinButton(false, true)
@@ -192,10 +308,23 @@ local function SkinPerksProgram()
         PerksProgramFrame.FooterFrame.PurchaseButton:HookScript("OnEnter", PurchaseButton_OnEnter)
         PerksProgramFrame.FooterFrame.PurchaseButton:HookScript("OnLeave", PurchaseButton_OnLeave)
 
+        if PerksProgramFrame.FooterFrame.ViewCartButton.ItemCountBG then
+            PerksProgramFrame.FooterFrame.ViewCartButton.ItemCountBG:GwStripTextures()
+        end
+
+        if PerksProgramFrame.FooterFrame.ViewCartButton.ItemCountText then
+            PerksProgramFrame.FooterFrame.ViewCartButton.ItemCountText:ClearAllPoints()
+            PerksProgramFrame.FooterFrame.ViewCartButton.ItemCountText:SetPoint("BOTTOMLEFT", 4, 2)
+        end
+
+        PerksProgramFrame.FooterFrame.ViewCartButton.texture = PerksProgramFrame.FooterFrame.ViewCartButton:CreateTexture(nil, "ARTWORK")
+        PerksProgramFrame.FooterFrame.ViewCartButton.texture:SetAtlas("Perks-ShoppingCart")
+        PerksProgramFrame.FooterFrame.ViewCartButton.texture:GwSetInside(nil, 8, 8)
+        PerksProgramFrame.FooterFrame.ViewCartButton.texture:SetDesaturated(true)
+
         -- handle the glow
         hooksecurefunc(GlowEmitterFactory, "Show", GlowEmitterFactory_Show)
         hooksecurefunc(GlowEmitterFactory, "Hide", GlowEmitterFactory_Hide)
-
     end
 end
 
