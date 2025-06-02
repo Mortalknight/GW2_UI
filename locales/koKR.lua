1b32652256f82a7020251c15d7a7f2612c71cac4
Nils89 <nils-reinecke@hotmail.de>
2025-06-02 11:43:05 +0200

    again

--- locales/koKR.lua
+++ locales/koKR.lua
@@ -2,7 +2,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("GW2_UI", "koKR")
 if not L then return end
 
--- Timestamp
-L["TimeStamp m/d h:m:s"] = "%m/%d %H:%M:%S"
 --Strings
 --@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="ignore")@
