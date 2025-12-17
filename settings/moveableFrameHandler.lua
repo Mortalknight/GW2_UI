cd8b9a472221fd3441ddad20dc0d0cb61c6deee9
Nils89 <nils-reinecke@hotmail.de>
2025-12-17 11:47:37 +0100

    titan

--- settings/moveableFrameHandler.lua
+++ settings/moveableFrameHandler.lua
@@ -547,7 +547,7 @@ local function moverframe_OnLeave(self)
 end
 
 local function CreateMoverFrame(parent, displayName, settingsName, size, frameOptions, mhf, postdrag, tags)
-    local mf = CreateFrame("Button", "Gw_" .. settingsName, UIParent)
+    local mf = CreateFrame("Button", "Gw_" .. settingsName, UIParent, "SecureHandlerStateTemplate")
     mf:SetClampedToScreen(true)
     mf:SetMovable(true)
     mf:EnableMouseWheel(true)
@@ -556,7 +556,6 @@ local function CreateMoverFrame(parent, displayName, settingsName, size, frameOp
     mf:SetFrameStrata("DIALOG")
     mf:GwCreateBackdrop("Transparent White")
     mf:SetScale(parent:GetScale())
-    mf:SetProtected()
 
     if size then
         mf:SetSize(unpack(size))
